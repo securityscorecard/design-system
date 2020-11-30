81d5461a6630e0c88c09f130031749e57ea80f13
Alexey Bykov <abykov@securityscorecard.io>
2020-11-30 18:07:22 +0100

    test(visual-regressions): bump reg-cli and remove workaround

--- visual-regressions/bin/update.sh
+++ visual-regressions/bin/update.sh
@@ -4,5 +4,3 @@ rm -rf "$SNAPSHOT_DIR/diff"
 rm "$SNAPSHOT_DIR/report.html"
 rm "$SNAPSHOT_DIR/report.json"
 reg-cli "$SNAPSHOT_DIR"/actual "$SNAPSHOT_DIR"/expected "$SNAPSHOT_DIR"/diff -M 0.01 -A -E -U -R "$SNAPSHOT_DIR"/report.html -J "$SNAPSHOT_DIR"/report.json
-# reg-cli update is buggy as hell, so we manually try to copy as many files as possible to prevent any errors
-./bin/apply-changes.js
