f46d886aff096359973f2e54f1c4883a4f9c0d5d
Radek Podrazky <rpodrazky@securityscorecard.io>
2021-01-13 17:09:33 +0100

    feat(Datatable): add BatchActions component

---
+++ src/components/Datatable/BatchActions/BatchActions.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { isNotUndefined } from 'ramda-adjunct';
+
+import { createPaddingSpacing } from '../../../utils/helpers';
+import { FlexContainer } from '../../FlexContainer';
+import { Button } from '../../Button';
+import { ButtonVariants } from '../../Button/Button.enums';
+import {
+  AbsoluteLinkActionKind,
+  ActionPropType,
+  ActionWithSubactions,
+  RelativeLinkActionKind,
+} from '../types/Action.types';
+import { BatchActionsProps } from './BatchActions.types';
+import { Dropdown } from '../Dropdown';
+import { Icon } from '../../Icon';
+import { SSCIconNames } from '../../Icon/Icon.enums';
+import { ColorTypes } from '../../../theme/colors.enums';
+
+const BatchActionButton = styled(Button).attrs((props) => ({
+  variant: ButtonVariants.text,
+  ...props,
+}))`
+  ${createPaddingSpacing({ horizontal: 0.8 })}
+`;
+
+const BatchActions: React.FC<BatchActionsProps> = ({ actions }) => (
+  <FlexContainer>
+    {actions.map((action) => {
+      if (isNotUndefined((action as ActionWithSubactions).subActions)) {
+        return (
+          <Dropdown
+            key={action.name}
+            actions={(action as ActionWithSubactions).subActions}
+          >
+            <BatchActionButton name={action.name} onClick={action.onClick}>
+              {action.label}{' '}
+              <Icon
+                color={ColorTypes.graphite2B}
+                margin={{ left: 0.25 }}
+                name={SSCIconNames.caretDown}
+              />
+            </BatchActionButton>
+          </Dropdown>
+        );
+      }
+
+      return (
+        <BatchActionButton
+          key={action.name}
+          href={(action as AbsoluteLinkActionKind).href}
+          name={action.name}
+          to={(action as RelativeLinkActionKind).to}
+          onClick={action.onClick}
+        >
+          {action.label}
+        </BatchActionButton>
+      );
+    })}
+  </FlexContainer>
+);
+
+BatchActions.propTypes = {
+  actions: PropTypes.arrayOf(ActionPropType),
+};
+
+export default BatchActions;
