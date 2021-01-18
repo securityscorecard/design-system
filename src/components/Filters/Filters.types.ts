c7f4797ffc167fbfb63182f0be107ef67c754608
Adam <ablazek@securityscorecard.io>
2021-01-18 12:48:44 +0100

    feat(Filters): update component api

--- src/components/Filters/Filters.types.ts
+++ src/components/Filters/Filters.types.ts
@@ -1,13 +1,20 @@
 import { Option } from './Select/Select.types';
+import { InputTypes } from './Filters.enums';
+
+type InputValueTypes = typeof InputTypes[keyof typeof InputTypes];
 
 export interface FilterRow {
   id: string;
   conditionOptions: Option[];
-  dataOptions: Option[];
+  defaultCondition?: Option;
   onRemove: () => void;
+  inputType: InputValueTypes;
+  enumOptions?: Option[];
   isFilterApplied?: boolean;
+  onAdd?: () => void;
 }
 
 export interface FilterProps {
-  options: FilterRow[];
+  dataOptions: Option[];
+  rows: FilterRow[];
 }
