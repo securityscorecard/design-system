cc10a91daf6afd1da8da643b5dce98cf74f38efb
Adam <ablazek@securityscorecard.io>
2021-02-15 14:05:30 +0100

    feat(Filters): update storybook

--- src/components/Filters/mocks/options.ts
+++ src/components/Filters/mocks/options.ts
@@ -8,20 +8,95 @@ import {
 import { Field, Filter } from '../Filters.types';
 import { Operators } from '../Filters.enums';
 
-// TODO use more realistic data
-export const fieldsMock: Field[] = [
+export const fields: Field[] = [
   {
     conditions: [
-      { component: Input, value: 'chocolate', label: 'Chocolate' },
+      { component: Input, value: 'is', label: 'is' },
       {
         component: Input,
-        value: 'strawberry',
-        label: 'Strawberry',
+        value: 'is not',
+        label: 'is not',
       },
-      { component: Input, value: 'vanilla', label: 'Vanilla' },
+      {
+        component: Input,
+        value: 'contains',
+        label: 'contains',
+        isDefault: true,
+      },
+      {
+        component: Input,
+        value: 'does not contain',
+        label: 'does not contain',
+      },
     ],
-    value: 'pistachio',
-    label: 'Pistachio',
+    value: 'domain',
+    label: 'Domain',
+  },
+  {
+    conditions: [
+      {
+        component: Number,
+        value: 'is',
+        label: 'is',
+        isDefault: true,
+      },
+      {
+        component: Number,
+        value: 'is not',
+        label: 'is not',
+      },
+      {
+        component: Number,
+        value: 'is less than',
+        label: 'is less than',
+      },
+      {
+        component: Number,
+        value: 'is greater than',
+        label: 'is greater than',
+      },
+      {
+        component: Number,
+        value: 'is at least',
+        label: 'is at least',
+      },
+      {
+        component: Number,
+        value: 'is at most',
+        label: 'is at most',
+      },
+    ],
+    value: 'issue count',
+    label: 'Issue Count',
+  },
+  {
+    conditions: [
+      { component: SingleDatePicker, value: 'is', label: 'is' },
+      { component: SingleDatePicker, value: 'before', label: 'before' },
+      {
+        component: SingleDatePicker,
+        value: 'after',
+        label: 'after',
+        isDefault: true,
+      },
+      {
+        component: SingleDatePicker,
+        value: 'within last',
+        label: 'within last',
+      },
+      {
+        component: SingleDatePicker,
+        value: 'not within last',
+        label: 'not within last',
+      },
+      {
+        component: DateRangePicker,
+        value: 'between',
+        label: 'between',
+      },
+    ],
+    value: 'first observed',
+    label: 'First Observed',
   },
   {
     conditions: [
@@ -30,74 +105,109 @@ export const fieldsMock: Field[] = [
           component: Select,
           props: {
             options: [
-              { value: 'foo', label: 'Foo' },
-              { value: 'bar', label: 'Bar' },
+              { value: 'any', label: 'Any' },
+              { value: 'none', label: 'None' },
+              { value: 'cloud', label: 'Cloud' },
+              { value: 'ssc res', label: 'SSC Res.' },
+              { value: '3rd party', label: '3rd party' },
             ],
-            defaultValue: { value: 'foo', label: 'Foo' },
+            defaultValue: { value: 'any', label: 'Any' },
           },
         },
-        value: 'chocolate',
-        label: 'Chocolate',
+        value: 'is',
+        label: 'is',
       },
-      { component: Input, value: 'strawberry', label: 'Strawberry' },
-      { component: Input, value: 'vanilla', label: 'Vanilla', isDefault: true },
-    ],
-    value: 'lemon',
-    label: 'Lemon',
-  },
-  {
-    conditions: [
       {
-        component: Number,
-        value: 'chocolate',
-        label: 'Chocolate',
+        component: {
+          component: Select,
+          props: {
+            options: [
+              { value: 'any', label: 'Any' },
+              { value: 'none', label: 'None' },
+              { value: 'cloud', label: 'Cloud' },
+              { value: 'ssc res', label: 'SSC Res.' },
+              { value: '3rd party', label: '3rd party' },
+            ],
+            defaultValue: { value: 'any', label: 'Any' },
+          },
+        },
+        value: 'is not',
+        label: 'is not',
+      },
+      {
+        component: {
+          component: Select,
+          props: {
+            options: [
+              { value: 'any', label: 'Any' },
+              { value: 'none', label: 'None' },
+              { value: 'cloud', label: 'Cloud' },
+              { value: 'ssc res', label: 'SSC Res.' },
+              { value: '3rd party', label: '3rd party' },
+            ],
+            defaultValue: { value: 'any', label: 'Any' },
+          },
+        },
+        value: 'contains',
+        label: 'contains',
         isDefault: true,
       },
-      { component: SingleDatePicker, value: 'strawberry', label: 'Strawberry' },
-      { component: DateRangePicker, value: 'vanilla', label: 'Vanilla' },
+      {
+        component: {
+          component: Select,
+          props: {
+            options: [
+              { value: 'any', label: 'Any' },
+              { value: 'none', label: 'None' },
+              { value: 'cloud', label: 'Cloud' },
+              { value: 'ssc res', label: 'SSC Res.' },
+              { value: '3rd party', label: '3rd party' },
+            ],
+            defaultValue: { value: 'any', label: 'Any' },
+          },
+        },
+        value: 'does not contain',
+        label: 'does not contain',
+      },
     ],
-    value: 'smurf',
-    label: 'Smurf',
+    value: 'detection method',
+    label: 'Detection Method',
   },
 ];
 
-export const stateMock: Filter[] = [
+export const state: Filter[] = [
   {
     operator: Operators.and,
-    field: 'pistachio',
-    condition: 'chocolate',
-    value: 'tasty',
+    field: 'domain',
+    condition: 'is not',
+    value: 'securityscorecard.io',
     isApplied: true,
   },
   {
     operator: Operators.and,
-    field: 'lemon',
-    condition: 'strawberry',
+    field: 'issue count',
+    condition: 'is greater than',
+    value: '2',
     isApplied: true,
   },
   {
     operator: Operators.and,
-    field: 'smurf',
-    condition: 'vanilla',
+    field: 'first observed',
+    condition: 'after',
+    value: new Date(),
     isApplied: true,
   },
   {
     operator: Operators.and,
-    field: 'smurf',
-    condition: 'strawberry',
+    field: 'first observed',
+    condition: 'between',
     isApplied: true,
   },
   {
     operator: Operators.and,
-    field: 'smurf',
-    condition: 'chocolate',
-    isApplied: true,
-  },
-  {
-    operator: Operators.and,
-    field: 'lemon',
-    condition: 'chocolate',
-    value: 'foo',
+    field: 'detection method',
+    condition: 'is not',
+    value: 'ssc res',
     isApplied: true,
   },
 ];
