d317f75484b68d75f642c8a6fc84e79cb7709d66
Youssef Faltas <yfaltas@securityscorecard.io>
2021-02-23 15:46:25 +0100

    feat(SearchBox): make hasSuggestions optional

--- src/components/Filters/components/Input/Input.tsx
+++ src/components/Filters/components/Input/Input.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import { noop } from 'ramda-adjunct';
 
 import { InputProps } from './Input.types';
 import {
