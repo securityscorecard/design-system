7bea82df200db89a46626c96f334fe8fe550e5d2
Adam <ablazek@securityscorecard.io>
2021-03-02 09:20:14 +0100

    feat(Filters): add new stories to storybook

---
+++ src/components/Filters/components/DateRangePicker/DateRangePicker.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { action } from '@storybook/addon-actions';
+
+import Filters from '../../Filters';
+import { FlexContainer } from '../../../FlexContainer';
+import {
+  fieldsDateRange,
+  fieldsDateRangeValidation,
+} from '../../mocks/options';
+
+export default {
+  component: Filters,
+  title: 'components/Filters/components/DateRangePicker',
+} as Meta;
+
+export const DateRange: Story = () => (
+  <FlexContainer margin={{ left: 3 }}>
+    <Filters
+      fields={fieldsDateRange}
+      onApply={action('onApply')}
+      onCancel={action('onCancel')}
+      onChange={action('onChange')}
+      onClose={action('onClose')}
+    />
+  </FlexContainer>
+);
+
+export const DateRangeValidation: Story = () => (
+  <FlexContainer margin={{ left: 3 }}>
+    <Filters
+      fields={fieldsDateRangeValidation}
+      onApply={action('onApply')}
+      onCancel={action('onCancel')}
+      onChange={action('onChange')}
+      onClose={action('onClose')}
+    />
+  </FlexContainer>
+);
