9b37589c3f5cee6dd44ad5fe23ddb79a62fcc82a
Radek Podrazky <rpodrazky@securityscorecard.io>
2021-03-29 14:40:04 +0200

    chore: update repo setup

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,11 @@
 import '@testing-library/jest-dom';
 import 'jest-styled-components';
+import { createIconLibrary, resetIconLibrary } from './src';
+
+beforeAll(() => {
+  createIconLibrary();
+});
+
+afterAll(() => {
+  resetIconLibrary();
+});
