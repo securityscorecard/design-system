b0e150f16544f569dc8c0b6f5e44e56262c2bcc8
Radek Podrazky <rpodrazky@securityscorecard.io>
2021-03-30 13:52:02 +0200

    feat(Datatable): add BatchModule

---
+++ src/components/DatatableV2/BatchModule/BatchModule.types.ts
@@ -0,0 +1,6 @@
+import { BatchActionsProps } from './BatchActions/BatchActions.types';
+import { ElementCounterProps } from './ElementCounter/ElementCounter.types';
+
+export interface BatchModuleProps
+  extends BatchActionsProps,
+    ElementCounterProps {}
