4e89542ea73436ef6381e48e6740b79c74f50ef4
Radek Podrazky <rpodrazky@securityscorecard.io>
2021-03-30 17:23:13 +0200

    feat(Datatable): add table footer

---
+++ src/components/DatatableV2/Table/Footer/GoToPage.test.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { renderWithProviders } from '../../../../utils/tests/renderWithProviders';
+import GoToPage from './GoToPage';
+
+const pageChangeMock = jest.fn();
+
+describe('Datatable/GoToPage', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+  it('should call onPageChange handler with "1" when value is less than or equal to 0', () => {
+    renderWithProviders(
+      <GoToPage pageCount={8} onPageChange={pageChangeMock} />,
+    );
+
+    userEvent.type(
+      screen.getByRole('spinbutton', { name: /Go to page:/i }),
+      '-5{enter}',
+    );
+
+    expect(pageChangeMock).toHaveBeenCalledWith(1);
+  });
+  it('should call onPageChange handler with actual value when value is in page count interval', () => {
+    const page = 4;
+    renderWithProviders(
+      <GoToPage pageCount={8} onPageChange={pageChangeMock} />,
+    );
+
+    userEvent.type(
+      screen.getByRole('spinbutton', { name: /Go to page:/i }),
+      `${page}{enter}`,
+    );
+
+    expect(pageChangeMock).toHaveBeenCalledWith(page);
+  });
+  it('should call onPageChange handler with highest page number value when value is greater than page count', () => {
+    const pageCount = 8;
+    renderWithProviders(
+      <GoToPage pageCount={pageCount} onPageChange={pageChangeMock} />,
+    );
+
+    userEvent.type(
+      screen.getByRole('spinbutton', { name: /Go to page:/i }),
+      '15{enter}',
+    );
+
+    expect(pageChangeMock).toHaveBeenCalledWith(pageCount);
+  });
+  it('should display "1" when provided value is less than or equal to 0', () => {
+    renderWithProviders(
+      <GoToPage pageCount={8} onPageChange={pageChangeMock} />,
+    );
+
+    const input = screen.getByRole('spinbutton', {
+      name: /Go to page:/i,
+    }) as HTMLInputElement;
+    userEvent.type(input, '-5{enter}');
+
+    expect(input.value).toBe('1');
+  });
+  it('should display actual provided value when is in page count interval', () => {
+    const page = 4;
+    renderWithProviders(
+      <GoToPage pageCount={8} onPageChange={pageChangeMock} />,
+    );
+
+    const input = screen.getByRole('spinbutton', {
+      name: /Go to page:/i,
+    }) as HTMLInputElement;
+    userEvent.type(input, `${page}{enter}`);
+
+    expect(input.value).toBe(`${page}`);
+  });
+  it('should display highest page number value when provided value is greater than page count', () => {
+    const pageCount = 8;
+    renderWithProviders(
+      <GoToPage pageCount={pageCount} onPageChange={pageChangeMock} />,
+    );
+
+    const input = screen.getByRole('spinbutton', {
+      name: /Go to page:/i,
+    }) as HTMLInputElement;
+    userEvent.type(input, '15{enter}');
+
+    expect(input.value).toBe(`${pageCount}`);
+  });
+});
---
+++ src/components/DatatableV2/Table/Footer/Pagination/Pagination.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { SSCIconNames } from '../../../../../theme/icons/icons.enums';
+import { Icon } from '../../../../Icon';
+import { PaginationItem } from './PaginationItem';
+import PageButtons from './PageButtons';
+import { FlexContainer } from '../../../../FlexContainer';
+import { PaginationProps } from './Pagination.types';
+
+const Pagination: React.FC<PaginationProps> = ({
+  pageCount,
+  currentPage,
+  onPageChange,
+  pageButtonsCount = 8,
+}) => (
+  <FlexContainer as="nav" flexDirection="row" justifyContent="center">
+    <PaginationItem
+      aria-label="Previous page"
+      isDisabled={currentPage - 1 === 0}
+      isShrinked
+      onClick={() => onPageChange(currentPage - 1)}
+    >
+      <Icon name={SSCIconNames.longArrowLeft} />
+    </PaginationItem>
+    <PageButtons
+      currentPage={currentPage}
+      pageCount={pageCount}
+      positions={pageButtonsCount}
+      onChange={onPageChange}
+    />
+    <PaginationItem
+      aria-label="Next page"
+      isDisabled={currentPage + 1 === pageCount}
+      isShrinked
+      onClick={() => onPageChange(currentPage + 1)}
+    >
+      <Icon name={SSCIconNames.longArrowRight} />
+    </PaginationItem>
+  </FlexContainer>
+);
+
+Pagination.propTypes = {
+  pageCount: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  pageButtonsCount: PropTypes.number,
+};
+
+export default Pagination;
