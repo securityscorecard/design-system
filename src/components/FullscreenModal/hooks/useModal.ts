c4c3b68a10e122833fc1160b27020cc6a17b276c
Radek Podrazky <rpodrazky@securityscorecard.io>
2021-06-02 12:02:27 +0200

    fix(FullscreenModal): freeze body when modal is open
    Closes FEP-1787

--- src/components/FullscreenModal/hooks/useModal.ts
+++ src/components/FullscreenModal/hooks/useModal.ts
@@ -23,16 +23,18 @@ export const useModal = (el: React.ReactElement): (() => React.ReactPortal) => {
     if (isNull(containerRef.current)) return null;
     const { current } = containerRef;
 
+    document.body.style.overflow = 'hidden';
     current.style.position = 'absolute';
     current.style.zIndex = '1250';
     current.style.top = '0';
     current.style.left = '0';
 
     return () => {
-      current.style.position = 'static';
-      current.style.zIndex = 'auto';
-      current.style.top = 'auto';
-      current.style.left = 'auto';
+      document.body.style.removeProperty('overflow');
+      current.style.removeProperty('position');
+      current.style.removeProperty('z-index');
+      current.style.removeProperty('top');
+      current.style.removeProperty('left');
     };
   }, [portalsContainerId]);
 
