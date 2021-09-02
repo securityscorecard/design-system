cdc68315e5a3b28ca290d5b2cf1029fcbb6eced2
Adam <ablazek@securityscorecard.io>
2021-09-02 11:32:46 +0200

    fix(Banner): update layout and fix colors usage

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { isNotUndefined } from 'ramda-adjunct';
+import { transparentize } from 'polished';
 
 import { BannerProps } from './Banner.types';
 import { BannerVariants } from './Banner.enums';
@@ -13,9 +14,9 @@ import { IconTypes, SSCIconNames } from '../../theme/icons/icons.enums';
 import { getColor, pxToRem } from '../../utils';
 
 const colorVariants = {
-  [BannerVariants.info]: colors.radiantBlueberry,
-  [BannerVariants.warn]: colors.pumpkin,
-  [BannerVariants.error]: colors.strawberry,
+  [BannerVariants.info]: 'radiantBlueberry',
+  [BannerVariants.warn]: 'pumpkin',
+  [BannerVariants.error]: 'strawberry',
 };
 
 const iconVariants = {
@@ -25,19 +26,21 @@ const iconVariants = {
 };
 
 const bgVariants = {
-  [BannerVariants.info]: { r: 235, g: 244, b: 252, a: 0.5 },
-  [BannerVariants.warn]: { r: 240, g: 143, b: 0, a: 0.05 },
-  [BannerVariants.error]: { r: 241, g: 67, b: 28, a: 0.05 },
+  [BannerVariants.info]: { color: colors.blueberry0, alpha: 0.5 },
+  [BannerVariants.warn]: { color: colors.pumpkin, alpha: 0.95 },
+  [BannerVariants.error]: { color: colors.strawberry, alpha: 0.95 },
 };
 
-const StyledInline = styled(Inline)<Pick<BannerProps, 'variant'>>`
-  border: ${({ variant }) => `1px solid ${colorVariants[variant]}`};
+const StyledPadbox = styled(Padbox)<Pick<BannerProps, 'variant'>>`
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${({ variant }) => getColor(colorVariants[variant])};
   background: ${({ variant }) =>
-    `rgba(${bgVariants[variant].r}, ${bgVariants[variant].g}, ${bgVariants[variant].b}, ${bgVariants[variant].a})`};
+    transparentize(bgVariants[variant].alpha, bgVariants[variant].color)};
 `;
 
 const IconContainer = styled.div<Pick<BannerProps, 'variant'>>`
-  background-color: ${({ variant }) => colorVariants[variant]};
+  background-color: ${({ variant }) => getColor(colorVariants[variant])};
   height: 100%;
   display: flex;
   align-items: center;
@@ -47,6 +50,7 @@ const StyledIcon = styled(Icon)`
   color: ${colors.graphite5H};
 `;
 
+// TODO Extract close button and unify it across DS
 const CloseButton = styled.button`
   display: flex;
   align-items: center;
@@ -68,53 +72,55 @@ const Banner: React.FC<BannerProps> = ({
   cta2,
   onClose,
 }) => (
-  <StyledInline justify="space-between" variant={variant}>
-    <Inline align="center">
-      <IconContainer variant={variant}>
-        <Padbox paddingSize="lg" paddingType="squish">
-          <StyledIcon
-            name={iconVariants[variant]}
-            type={IconTypes.ssc}
-            hasFixedWidth
-          />
+  <StyledPadbox variant={variant}>
+    <Inline justify="space-between">
+      <Inline align="center">
+        <IconContainer variant={variant}>
+          <Padbox paddingSize="lg" paddingType="squish">
+            <StyledIcon
+              name={iconVariants[variant]}
+              type={IconTypes.ssc}
+              hasFixedWidth
+            />
+          </Padbox>
+        </IconContainer>
+        <Padbox paddingSize="md" paddingType="stretch">
+          {children}
         </Padbox>
-      </IconContainer>
-      <Padbox paddingSize="md" paddingType="stretch">
-        {children}
-      </Padbox>
-    </Inline>
-    <Inline align="center">
-      <Inline align="center" gap="lg">
-        {isNotUndefined(cta1) && (
-          <Button
-            color="primary"
-            href={cta1.href}
-            to={cta1.to}
-            variant="text"
-            onClick={cta1.onClick}
-          >
-            {cta1.text}
-          </Button>
-        )}
-        {isNotUndefined(cta2) && (
-          <Button
-            color="primary"
-            href={cta2.href}
-            to={cta2.to}
-            variant="text"
-            onClick={cta2.onClick}
-          >
-            {cta2.text}
-          </Button>
-        )}
       </Inline>
-      <Padbox paddingSize="md">
-        <CloseButton aria-label="Close" onClick={onClose}>
-          <TimesIcon name={SSCIconNames.times} type={IconTypes.ssc} />
-        </CloseButton>
-      </Padbox>
+      <Inline align="center">
+        <Inline align="center" gap="lg">
+          {isNotUndefined(cta1) && (
+            <Button
+              color="primary"
+              href={cta1.href}
+              to={cta1.to}
+              variant="text"
+              onClick={cta1.onClick}
+            >
+              {cta1.text}
+            </Button>
+          )}
+          {isNotUndefined(cta2) && (
+            <Button
+              color="primary"
+              href={cta2.href}
+              to={cta2.to}
+              variant="text"
+              onClick={cta2.onClick}
+            >
+              {cta2.text}
+            </Button>
+          )}
+        </Inline>
+        <Padbox paddingSize="md">
+          <CloseButton aria-label="Close" onClick={onClose}>
+            <TimesIcon name={SSCIconNames.times} type={IconTypes.ssc} />
+          </CloseButton>
+        </Padbox>
+      </Inline>
     </Inline>
-  </StyledInline>
+  </StyledPadbox>
 );
 
 export default Banner;
