48f3ae7f2f1cccb6bcffb4e6b0bf06ead839d6f1
Adam <ablazek@securityscorecard.io>
2021-11-08 17:38:28 +0100

    feat(SimpleModal): add SimpleModal component

---
+++ src/components/Modal/Modal.types.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { ModalSizes } from './Modal.enums';
+
+export type Sizes = typeof ModalSizes[keyof typeof ModalSizes];
+
+export interface ModalProps {
+  children: React.ReactNode;
+  onClose: React.MouseEventHandler;
+  footer?: React.ReactNode;
+  title?: string;
+  size?: Sizes;
+}
