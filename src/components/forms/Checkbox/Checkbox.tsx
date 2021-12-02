4da24ffdf45bf20aa69313657f58a8ada247c9ab
dclerici-ssc <94005023+dclerici-ssc@users.noreply.github.com>
2021-12-02 16:16:13 -0300

    refactor: extend Checkbox and Radio label support

--- src/components/forms/Checkbox/Checkbox.tsx
+++ src/components/forms/Checkbox/Checkbox.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import { add, allPass, identity, memoizeWith, pipe } from 'ramda';
-import { isNonEmptyString, isNotUndefined } from 'ramda-adjunct';
+import { isNotUndefined } from 'ramda-adjunct';
 
 import * as checked from '../../../theme/icons/check';
 import * as indeterminate from '../../../theme/icons/minus';
@@ -143,7 +143,7 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
     },
     ref,
   ) => {
-    const hasLabel = allPass([isNotUndefined, isNonEmptyString])(label);
+    const hasLabel = allPass([isNotUndefined])(label);
 
     return (
       <CheckboxWrapper className={className}>
@@ -179,7 +179,7 @@ const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
 Checkbox.propTypes = {
   name: PropTypes.string.isRequired,
   checkboxId: PropTypes.string,
-  label: PropTypes.string,
+  label: PropTypes.node || PropTypes.string,
   className: PropTypes.string,
   isDisabled: PropTypes.bool,
   isInvalid: PropTypes.bool,
