e5a094b67bdcc4609c7f9382e09949fb2655a97d
Alexey Bykov <abykov@securityscorecard.io>
2022-01-14 17:18:04 +0300

    ci: enable visual regressions for m1 macs

--- visual-regressions/bin/take-and-check-in-docker-ci.sh
+++ visual-regressions/bin/take-and-check-in-docker-ci.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 echo "building image";
-localImage="$(docker build -q visual-regressions)"
+localImage="$(docker build -q --platform=linux/amd64 visual-regressions)"
 echo "built image: ${localImage}"
-docker run --rm --network host -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" $localImage yarn serve-take-and-check
+docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 $localImage yarn serve-take-and-check
--- visual-regressions/bin/take.sh
+++ visual-regressions/bin/take.sh
@@ -2,5 +2,5 @@
 export PATH="$PATH:../node_modules/.bin/"
 actual="$SNAPSHOT_DIR/actual"
 rm -rf "$actual"
-storycap "$STORYBOOK_URL" -f -o "$actual" --disableCssAnimation
+storycap "$STORYBOOK_URL" -f -o "$actual" --disableCssAnimation --captureTimeout 10000 --puppeteerLaunchConfig "{\"headless\":true,\"args\":[\"--no-sandbox\",\"--disable-setuid-sandbox\",\"--disable-dev-shm-usage\", \"--disable-gpu\"]}"
 cd "$actual" || exit 1
