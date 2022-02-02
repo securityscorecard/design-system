40232d8053a39d2f436d894900a4e7fc67937a1b
Alexey Bykov <abykov@securityscorecard.io>
2022-02-02 18:11:45 +0300

    ci: use prebuilt image for visual regressions

--- visual-regressions/bin/take-and-check-in-docker-ci.sh
+++ visual-regressions/bin/take-and-check-in-docker-ci.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env sh
-echo "building image";
-localImage="$(docker build -q --platform=linux/amd64 visual-regressions)"
-echo "built image: ${localImage}"
-docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 $localImage yarn serve-take-and-check
+docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 abykovssc/visual-regressions:multi-arch yarn serve-take-and-check
