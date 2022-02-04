29f5303d7eb81cf82803f7600fb3332d9101d419
Alexey Bykov <abykov@securityscorecard.io>
2022-02-04 12:30:07 +0300

    ci: use ubuntu and chrome 97 for visual regressions

--- visual-regressions/bin/take-and-check-in-docker-ci.sh
+++ visual-regressions/bin/take-and-check-in-docker-ci.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 abykovssc/visual-regressions:multi-arch yarn serve-take-and-check
+docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 abykovssc/visual-regressions:multi-arch-ubuntu yarn serve-take-and-check
