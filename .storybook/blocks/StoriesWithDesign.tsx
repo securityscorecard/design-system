966861ebf399e0d68c8cdcae847b69e3095f811f
Radek Podrazky <rpodrazky@securityscorecard.io>
2022-03-08 15:15:45 +0100

    chore(package): upgrade Storybook to v6.5

--- .storybook/blocks/StoriesWithDesign.tsx
+++ .storybook/blocks/StoriesWithDesign.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, Fragment } from 'react';
+import React, { useContext } from 'react';
 import {
   DocsContext,
   DocsStory,
@@ -26,21 +26,9 @@ interface StoriesProps {
   includePrimary?: boolean;
 }
 
-const getDocsStories = (context: DocsContextProps): StoryData[] => {
-  const { storyStore, kind } = context;
-
-  if (!storyStore) {
-    return [];
-  }
-
-  return storyStore
-    .getStoriesForKind(kind)
-    .filter((s: any) => !(s.parameters?.docs?.disable));
-};
-
 export const StoriesWithDesign: React.FC<StoriesProps> = ({ title, includePrimary = false }) => {
   const context = useContext(DocsContext);
-  const componentStories = getDocsStories(context);
+  const componentStories = context.componentStories().filter((s: any) => !(s.parameters?.docs?.disable));;
 
   let stories: DocsStoryProps[] = componentStories;
   if (!includePrimary) stories = stories.slice(1);
