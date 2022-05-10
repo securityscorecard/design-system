2b5721b55e91da13fe48208f4711c1282eec5b9f
Julian Barrionuevo <jbarrionuevo@securityscorecard.io>
2022-05-10 16:15:36 -0300

    fix: heading exports

--- src/components/typography/Heading/index.ts
+++ src/components/typography/Heading/index.ts
@@ -1,2 +1,2 @@
 export * as HeadingEnums from './Heading.enums';
-export { default as Heading, H0, H1, H2, H3, H4 } from './Heading';
+export { default as Heading, H0, H1, H2, H3, H4, H5 } from './Heading';
