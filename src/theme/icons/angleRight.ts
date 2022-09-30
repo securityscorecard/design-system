25cc55e0ffd7eea8086b6626374845bc9d89f7f2
Natalie Mikesova <nmikesova@securityscorecard.io>
2022-09-30 12:17:41 +0200

    feat(Icons): add angle icons

---
+++ src/theme/icons/angleRight.ts
@@ -0,0 +1,23 @@
+/*!
+ * Font Awesome Free 6.1.2 by @fontawesome - https://fontawesome.com
+ * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
+ * Copyright 2022 Fonticons, Inc.
+ */
+
+import {
+  IconDefinition,
+  IconName,
+  IconPrefix,
+} from '@fortawesome/fontawesome-svg-core';
+
+export const width = 320;
+export const height = 512;
+export const unicode = 'e02c';
+export const svgPathData =
+  'M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z';
+
+export const angleRight = {
+  prefix: 'ssc' as IconPrefix,
+  iconName: 'angle-right' as IconName,
+  icon: [width, height, [], unicode, svgPathData],
+} as IconDefinition;
