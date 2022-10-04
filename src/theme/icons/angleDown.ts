f8c244f0d7c1ca2879b12b854a4bb99d7ebcdd8c
Natalie Mikesova <nmikesova@securityscorecard.io>
2022-10-04 13:27:54 +0200

    feat(AngleDownIon): fix the dimensions

--- src/theme/icons/angleDown.ts
+++ src/theme/icons/angleDown.ts
@@ -10,8 +10,8 @@ import {
   IconPrefix,
 } from '@fortawesome/fontawesome-svg-core';
 
-export const width = 512;
-export const height = 448;
+export const width = 448;
+export const height = 512;
 export const unicode = 'e02f';
 export const svgPathData =
   'M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z';
