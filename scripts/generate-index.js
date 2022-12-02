277de8f44394f6ef62da35fbe4d72295861a4011
Radek Podrazky <rpodrazky@securityscorecard.io>
2022-12-02 17:55:15 +0100

    build(docs): run docs deploy on release

--- scripts/generate-index.js
+++ scripts/generate-index.js
@@ -3,23 +3,28 @@ const fs = require('fs');
 const path = require('path');
 /* eslint-enable @typescript-eslint/no-var-requires */
 
-const docsRootPath = path.resolve(__dirname, '../docs-build/index.html');
+const docsRootPath = path.resolve(__dirname, '../docs-build');
+const alphaDocsRootPath = path.resolve(__dirname, '../docs-build/alpha');
 const version = process.env.VERSION;
 const url = `https://securityscorecard.github.io/design-system/${version}`;
 const content = `<!DOCTYPE HTML>
 <html lang="en-US">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=${url}">
-        <script type="text/javascript">
-            window.location.href = "${url}"
-        </script>
-        <title>Page Redirection</title>
-    </head>
-    <body>
-        If you are not redirected automatically, use this <a href='${url}'>link</a>.
-    </body>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="1; url=${url}">
+    <script type="text/javascript">
+      window.location.href = "${url}"
+    </script>
+    <title>Page Redirection</title>
+  </head>
+  <body>
+    If you are not redirected automatically, use this <a href='${url}'>link</a>.
+  </body>
 </html>
 `;
 
-fs.writeFileSync(docsRootPath, content);
+fs.mkdirSync(alphaDocsRootPath, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+fs.writeFileSync(`${docsRootPath}/index.html`, content);
+fs.writeFileSync(`${alphaDocsRootPath}/index.html`, content);
