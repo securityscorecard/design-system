8a74150b78676d548eecead4d37f140b47ee2154
OldrichKruchna <okruchna@securityscorecard.io>
2023-01-04 13:22:36 +0100

    feat: add same content height, hide non active steps in wizard

--- src/components/Wizard/WizardStep.tsx
+++ src/components/Wizard/WizardStep.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 import { useRegisterStep } from './hooks/useRegisterStep';
 import { WizardActionPropType, WizardStepProps } from './Wizard.types';
 import { useActiveStep } from './hooks/useActiveStep';
+import { pxToRem } from '../../utils';
+
+const StepContainer = styled.div`
+  height: ${pxToRem(418)};
+`;
 
 const WizardStep: React.FC<WizardStepProps> = ({
   children,
@@ -12,7 +18,7 @@ const WizardStep: React.FC<WizardStepProps> = ({
   const activeStep = useActiveStep();
   const isActiveStep = activeStep?.id === step.id;
   useRegisterStep(step);
-  return isActiveStep ? <div>{children}</div> : null;
+  return isActiveStep ? <StepContainer>{children}</StepContainer> : null;
 };
 
 WizardStep.propTypes = {
