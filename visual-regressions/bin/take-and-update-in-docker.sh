7891b9e7cee3d22a193f850b68c05994caf9c29a
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-03-21 11:02:06 +0100

    chore: upgrade Storybook to v7
    BREAKING CHANGE:
    This PR contains many dependency upgrades which may cause dependency incompatibilities.
    
    Ref UXD-1108

--- visual-regressions/bin/take-and-update-in-docker.sh
+++ visual-regressions/bin/take-and-update-in-docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run --rm --network host -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -it abykovssc/visual-regressions:multi-arch-ubuntu yarn take-and-update
+docker run --rm --network host -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -it abykovssc/visual-regressions:multi-arch-ubuntu-v2 yarn take-and-update
