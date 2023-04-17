e9a33dc2095f3df9bc95f9a9acdda9fa8cec1b05
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-04-17 14:27:32 +0200

    Merge branch 'main' into ajkl2533@merge-111-to-next-major

--- src/managers/BannersManager/BannersProvider.tsx
+++ src/managers/BannersManager/BannersProvider.tsx
@@ -1,6 +1,6 @@
+import type { ReactNode } from 'react';
 import type { BannerInstance, BannersContextProps } from './types';
 import type { InstanceId } from '../common/types';
-import type { ReactNode } from 'react';
 
 import { useMemo } from 'react';
 
