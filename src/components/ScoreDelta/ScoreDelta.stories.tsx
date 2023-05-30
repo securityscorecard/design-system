b6904ae3d9a2c17521cf8f6d062c94f58a8c8278
Julian Barrionuevo <jbarrionuevo@securityscorecard.io>
2023-05-30 12:34:44 -0300

    feat(TrendIcon): add component

--- src/components/ScoreDelta/ScoreDelta.stories.tsx
+++ src/components/ScoreDelta/ScoreDelta.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react/types-6-0';
 
-import ScoreDelta from '.';
+import ScoreDelta from './ScoreDelta';
 import { Inline } from '../layout';
 
 export default {
