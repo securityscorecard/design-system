6f0d53bba0ee46374fc134dd40246de6391c9554
Julian Barrionuevo <jbarrionuevo@securityscorecard.io>
2023-09-04 14:53:26 -0300

    fix(Link): update styling

--- src/components/_internal/BaseLink/BaseLink.ts
+++ src/components/_internal/BaseLink/BaseLink.ts
@@ -9,7 +9,8 @@ export const LinkBaseStyles = css<LinkStylesProps>`
   margin: 0;
   border: none;
   font-family: inherit;
-  font-weight: ${getFontWeight('semibold')};
+  font-weight: ${getFontWeight('regular')};
+  text-decoration: underline;
   background-color: transparent;
   white-space: nowrap;
   cursor: pointer;
@@ -17,15 +18,14 @@ export const LinkBaseStyles = css<LinkStylesProps>`
 `;
 export const LinkHoverStyles = css<LinkStylesProps>`
   color: ${(p) => getToken(`color-action-link-${p.$color}-hover`, p)};
-  text-decoration: underline;
 `;
 export const LinkFocusStyles = css<LinkStylesProps>`
   outline: 0;
   color: ${(p) => getToken(`color-action-link-${p.$color}-hover`, p)};
-  text-decoration: underline;
   background-color: ${(p) =>
-    getToken(`color-action-link-background${p.$color}-focus`, p)};
+    getToken(`color-action-link-background-${p.$color}-focus`, p)};
   border-radius: ${getRadii('default')};
+  text-decoration: none;
 `;
 export const LinkActiveStyles = css<LinkStylesProps>`
   color: ${(p) => getToken(`color-action-link-${p.$color}-active`, p)};
