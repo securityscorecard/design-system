783f00be86e1aee546021205b127e3af70557a6b
Mnau-mnau <nmikesova@securityscorecard.io>
2023-10-02 13:23:17 +0200

    feat(Icons): update icons to 6.4.2

--- src/theme/icons/minus.ts
+++ src/theme/icons/minus.ts
@@ -1,7 +1,7 @@
 /*!
- * Font Awesome Free 6.1.2 by @fontawesome - https://fontawesome.com
+ * Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
- * Copyright 2022 Fonticons, Inc.
+ * Copyright 2023 Fonticons, Inc.
  */
 
 /* solid - minus */
@@ -16,7 +16,7 @@ export const width = 448;
 export const height = 512;
 export const unicode = 'e010';
 export const svgPathData =
-  'M400 288h-352c-17.69 0-32-14.32-32-32.01s14.31-31.99 32-31.99h352c17.69 0 32 14.3 32 31.99S417.7 288 400 288z';
+  'M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z';
 
 export const minus = {
   prefix: 'ssc' as IconPrefix,
