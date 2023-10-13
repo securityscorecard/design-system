7ff2407853566799438cde10a60699c3d9861897
Mnau-mnau <nmikesova@securityscorecard.io>
2023-10-13 14:07:09 +0200

    feat(StatusDot): use success.500 for green

--- src/components/StatusDot/StatusDot.tsx
+++ src/components/StatusDot/StatusDot.tsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import { getRadii, pxToRem } from '../../utils';
 
 export const StatusDotColors = {
-  good: '100, 244, 81',
+  good: '73, 162, 90',
   bad: '242, 46, 67',
   neutral: '84, 129, 217',
   inactive: '216, 216, 216',
