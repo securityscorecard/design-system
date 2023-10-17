9e1afbcce783620dc6198bf53b7758038a6b2c56
Mnau-mnau <nmikesova@securityscorecard.io>
2023-10-17 12:03:50 +0200

    feat(Badge): remove icon and text variant

--- src/components/Badge/Badge.types.ts
+++ src/components/Badge/Badge.types.ts
@@ -1,15 +1,11 @@
-import { Types as IconTypes, SSCIcons } from '../Icon/Icon.types';
 import { BadgeVariants } from './Badge.enums';
 
 export type BadgeVariant = typeof BadgeVariants[keyof typeof BadgeVariants];
 
 export interface BadgeProps {
   count?: number;
-  text?: string;
   variant?: BadgeElementProps['$variant'];
   className?: string;
-  iconName?: SSCIcons | string;
-  iconType?: IconTypes | string;
 }
 
 export interface BadgeElementProps {
