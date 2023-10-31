df18aa5d1320039064d60eaf388e8933b1ce2461
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-10-31 16:50:27 +0100

    feat(Datatable): add sorting in column actions
    Ref UXD-1234

---
+++ src/components/DatatableV2/tests/mocks.ts
@@ -0,0 +1,19 @@
+import { DatatableColumnDef } from '../Datatable.types';
+
+type Data = {
+  name: string;
+  surname: string;
+  color: string;
+};
+
+export const data: Data[] = [
+  { name: 'Stephen', surname: 'Strange', color: 'blue' },
+  { name: 'Steve', surname: 'Rogers', color: 'blue' },
+  { name: 'Bruce', surname: 'Banner', color: 'green' },
+];
+
+export const columns: DatatableColumnDef<Data>[] = [
+  { accessorKey: 'name' },
+  { accessorKey: 'surname' },
+  { accessorKey: 'color' },
+];
