a86ccb260f28c46af5e25092b09bdc48db73f8a8
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-10-31 17:37:50 +0100

    feat(DatatableV2): implement column visibility
    Closes UXD-1273

---
+++ src/components/DatatableV2/stories/ColumnVisibility.stories.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { VisibilityState } from '@tanstack/react-table';
+
+import Datatable from '../Datatable';
+import Template, { Story, columns } from './Template';
+
+export default {
+  title: 'components/DatatableV2/ColumnVisibility',
+  component: Datatable,
+  parameters: {
+    screenshot: { skip: true },
+  },
+} as ComponentMeta<typeof Datatable>;
+
+export const ColumnVisibilityEnabled: Story = Template.bind({});
+ColumnVisibilityEnabled.args = Template.args;
+
+export const DisabledColumnVisibility: Story = Template.bind({});
+DisabledColumnVisibility.args = { ...Template.args, enableHiding: false };
+
+export const DisabledColumnVisibilityForColumn: Story = Template.bind({});
+DisabledColumnVisibilityForColumn.args = {
+  ...Template.args,
+  columns: [...columns, { accessorKey: 'country', enableHiding: false }],
+};
+
+export const InitialColumnVisibility: Story = Template.bind({});
+InitialColumnVisibility.args = {
+  ...Template.args,
+  initialState: {
+    columnVisibility: { organization_grade: false },
+  },
+};
+
+export const ColumnVisibilityManagedState: Story = (args) => {
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+
+  return (
+    <>
+      <Datatable
+        state={{ columnVisibility }}
+        onColumnVisibilityChange={setColumnVisibility}
+        {...args}
+      />
+      <div>
+        <strong>Debug:</strong>
+        <pre>{JSON.stringify(columnVisibility, null, 2)}</pre>
+      </div>
+    </>
+  );
+};
+ColumnVisibilityManagedState.args = Template.args;
---
+++ src/components/DatatableV2/tests/columnVisibility.test.tsx
@@ -0,0 +1,69 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+
+import { renderWithProviders } from '../../../utils/tests/renderWithProviders';
+import Datatable from '../Datatable';
+import { columns, data } from './mocks';
+
+describe('DatatableV2/columnVisibility', () => {
+  it('should have column visibility enabled by default', async () => {
+    renderWithProviders(<Datatable data={data} columns={columns} />);
+
+    await userEvent.click(
+      screen.getAllByRole('button', {
+        name: /Show column actions/i,
+      })[0],
+    );
+    expect(
+      screen.getByRole('button', {
+        name: /👀 Hide column/i,
+      }),
+    ).toBeInTheDocument();
+  });
+
+  describe('when is column visibility enabled', () => {
+    it('should hide column', async () => {
+      renderWithProviders(<Datatable data={data} columns={columns} />);
+
+      await userEvent.click(
+        screen.getAllByRole('button', {
+          name: /Show column actions/i,
+        })[0],
+      );
+      await userEvent.click(
+        screen.getByRole('button', {
+          name: /👀 Hide column/i,
+        }),
+      );
+      expect(
+        screen.queryByRole('columnheader', { name: 'name' }),
+      ).not.toBeInTheDocument();
+    });
+
+    it('should hide column visibility for specific column if in columns definition', async () => {
+      renderWithProviders(
+        <Datatable
+          data={data}
+          columns={[
+            columns[0],
+            columns[1],
+            { ...columns[2], enableHiding: false },
+          ]}
+          enableHiding
+        />,
+      );
+
+      await userEvent.click(
+        screen.getAllByRole('button', {
+          name: /Show column actions/i,
+        })[2],
+      );
+      expect(
+        screen.queryByRole('button', {
+          name: /👀 Hide column/i,
+        }),
+      ).not.toBeInTheDocument();
+    });
+  });
+});
