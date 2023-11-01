b198482b89778441205a23231f549bdeed7a690e
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-11-01 13:09:41 +0100

    feat(DatatableV2): implement column pinning
    Closes UXD-1274

--- src/components/DatatableV2/columns.utils.ts
+++ src/components/DatatableV2/columns.utils.ts
@@ -1,6 +1,8 @@
 import {
+  DatatableColumn,
   DatatableColumnDef,
   DatatableDefinedColumnDef,
+  DatatableInstance,
 } from './Datatable.types';
 
 export const prepareColumns = <D>({
@@ -13,3 +15,26 @@ export const prepareColumns = <D>({
     columnDefType: columnDef?.columnDefType ?? 'data',
     enableSorting: columnDef?.enableSorting ?? true,
   }));
+
+const getIsLastLeftPinnedColumn = <D>(
+  table: DatatableInstance<D>,
+  column: DatatableColumn<D>,
+) =>
+  column.getIsPinned() === 'left' &&
+  table.getLeftLeafColumns().length - 1 === column.getPinnedIndex();
+
+const getIsFirstRightPinnedColumn = <D>(column: DatatableColumn<D>) =>
+  column.getIsPinned() === 'right' && column.getPinnedIndex() === 0;
+
+export const getCommonCellStyles = <D>({
+  table,
+  column,
+}: {
+  table: DatatableInstance<D>;
+  column: DatatableColumn<D>;
+}) => ({
+  borderRight: getIsLastLeftPinnedColumn(table, column)
+    ? '1px solid black'
+    : 'none',
+  borderLeft: getIsFirstRightPinnedColumn(column) ? '1px solid black' : 'none',
+});
