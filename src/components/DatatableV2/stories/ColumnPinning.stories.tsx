90a6bb0cba346bc0ff1de923cfc078546f38c4b0
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-11-01 13:09:41 +0100

    feat(DatatableV2): implement column pinning
    Closes UXD-1274

---
+++ src/components/DatatableV2/stories/ColumnPinning.stories.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { ComponentMeta } from '@storybook/react';
+import { ColumnPinningState } from '@tanstack/react-table';
+
+import Datatable from '../Datatable';
+import Template, { Story, columns } from './Template';
+
+export default {
+  title: 'components/DatatableV2/ColumnPinning',
+  component: Datatable,
+  parameters: {
+    screenshot: { skip: true },
+  },
+} as ComponentMeta<typeof Datatable>;
+
+export const ColumnPinningEnabled: Story = Template.bind({});
+ColumnPinningEnabled.args = Template.args;
+
+export const DisabledColumnPinning: Story = Template.bind({});
+DisabledColumnPinning.args = { ...Template.args, enableColumnPinning: false };
+
+export const DisabledColumnPinningForColumn: Story = Template.bind({});
+DisabledColumnPinningForColumn.args = {
+  ...Template.args,
+  columns: [...columns, { accessorKey: 'country', enablePinning: false }],
+};
+
+export const InitialColumnPinning: Story = Template.bind({});
+InitialColumnPinning.args = {
+  ...Template.args,
+  initialState: {
+    columnPinning: { left: ['organization_grade'] },
+  },
+};
+
+export const ColumnPinningManagedState: Story = (args) => {
+  const [columnPinning, setColumnPinning] = useState<ColumnPinningState>({
+    left: ['organization_grade'],
+  });
+
+  return (
+    <>
+      <Datatable
+        state={{ columnPinning }}
+        onColumnPinningChange={setColumnPinning}
+        {...args}
+      />
+      <div>
+        <strong>Debug:</strong>
+        <pre>{JSON.stringify(columnPinning, null, 2)}</pre>
+      </div>
+    </>
+  );
+};
+ColumnPinningManagedState.args = Template.args;
