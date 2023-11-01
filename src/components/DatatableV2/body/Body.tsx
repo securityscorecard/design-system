dea745a3da281a401d29b495d58577bcb97de0ff
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-11-01 13:09:41 +0100

    feat(DatatableV2): implement column pinning
    Closes UXD-1274

--- src/components/DatatableV2/body/Body.tsx
+++ src/components/DatatableV2/body/Body.tsx
@@ -7,7 +7,7 @@ const Body = <D,>({ table }: { table: DatatableInstance<D> }) => {
   return (
     <tbody className="ds-table-body">
       {table.getRowModel().rows.map((row) => (
-        <BodyRow key={row.id} row={row} />
+        <BodyRow key={row.id} row={row} table={table} />
       ))}
     </tbody>
   );
