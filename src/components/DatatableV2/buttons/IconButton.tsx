dab1123ed1b4bc00a59a592f5a2d036b5661aa6d
Radek Podrazky <rpodrazky@securityscorecard.io>
2023-11-29 13:26:30 +0100

    feat(Datatable): add IconButton

---
+++ src/components/DatatableV2/buttons/IconButton.tsx
@@ -0,0 +1,59 @@
+import React, { ComponentProps, forwardRef } from 'react';
+import styled from 'styled-components';
+
+import { SSCIconNames } from '../../../theme/icons/icons.enums';
+import { getColor, getRadii } from '../../../utils';
+import { Icon } from '../../Icon';
+import { Padbox } from '../../layout';
+
+interface IconButtonProps extends ComponentProps<'button'> {
+  label: string;
+  iconName: typeof SSCIconNames[keyof typeof SSCIconNames];
+  iconProps?: Partial<ComponentProps<typeof Icon>>;
+}
+
+const IconButtonRoot = styled(Padbox)`
+  width: 1rem;
+  height: 1rem;
+  box-sizing: content-box;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+
+  cursor: pointer;
+  color: ${getColor('neutral.500')};
+  border-radius: ${getRadii('round')};
+  transition: var(--sscds-transition);
+
+  &:disabled {
+    cursor: auto;
+  }
+  &:not(:disabled):hover {
+    background: ${getColor('primary.50')};
+    color: ${getColor('text.primary')};
+  }
+  &:active {
+    background: ${getColor('primary.200')};
+    color: ${getColor('text.primary')};
+  }
+`;
+
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ iconName, label, iconProps, ...props }, ref) => {
+    return (
+      <IconButtonRoot
+        ref={ref}
+        aria-label={label}
+        as="button"
+        paddingSize="sm"
+        paddingType="squish"
+        type="button"
+        {...props}
+      >
+        <Icon name={iconName} {...iconProps} />
+      </IconButtonRoot>
+    );
+  },
+);
+
+export default IconButton;
