1dec284550823a79908169c89929bdfb824a6704
Mnau-mnau <nmikesova@securityscorecard.io>
2023-11-29 12:17:15 +0100

    feat(Checkbox): make checkboxId required

--- src/components/forms/Select/Select.styles.tsx
+++ src/components/forms/Select/Select.styles.tsx
@@ -485,6 +485,7 @@ export const Option: ComponentType<OptionProps<OptionType, boolean>> = (
       <components.Option {...props}>
         <Inline gap={SpaceSizes.sm}>
           <Checkbox
+            checkboxId={`select-${label}`}
             checked={isSelected}
             isDisabled={isDisabled}
             label={children}
