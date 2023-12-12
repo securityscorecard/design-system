a76f7c09bf9de52b4ac1d07381169e1f08f251bd
Mnau-mnau <nmikesova@securityscorecard.io>
2023-12-12 15:51:36 +0100

    feat(Loading): add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+import { Padbox, Stack } from '../layout';
+import { ProgressBar } from '../ProgressBar';
+import { Spinner } from '../Spinner';
+import { Text } from '../typographyLegacy';
+import { pxToRem } from '../../utils';
+import { LoadingRootProps } from './Loading.types';
+
+const LoadingRoot = styled(Padbox)<LoadingRootProps>`
+  display: flex;
+  align-items: center;
+  ${({ $height }) => css`
+    min-height: ${pxToRem($height)};
+  `};
+`;
+
+export const Loading = ({ progress, subject, message, testId, height }) => {
+  const text = message || (subject ? `${subject} loading` : 'Loading');
+
+  return (
+    <LoadingRoot $height={height} data-test-id={testId} paddingSize="lg">
+      <Stack gap="md" style={{ width: '100%', textAlign: 'center' }}>
+        {progress !== undefined && <ProgressBar size="thin" value={progress} />}
+        <Spinner dark />
+        <Text as="p" size="h5">
+          {text}
+        </Text>
+      </Stack>
+    </LoadingRoot>
+  );
+};
+
+Loading.propTypes = {
+  subject: PropTypes.string,
+  message: PropTypes.string, // override the whole message
+  progress: PropTypes.number,
+  testId: PropTypes.string,
+  height: PropTypes.number,
+};
