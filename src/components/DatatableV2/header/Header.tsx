58219c8e538a5038f00872c6301add8182ec62d1
Julian Barrionuevo <jbarrionuevo@securityscorecard.io>
2024-01-29 12:53:20 -0300

    fix(DatatableV2): fix selection bar z index

--- src/components/DatatableV2/header/Header.tsx
+++ src/components/DatatableV2/header/Header.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { DatatableInstance } from '../Datatable.types';
 import HeaderRow from './HeaderRow';
+import { getSelectedRowsCount } from '../toolbar/Selection';
 
 const Header = <D,>({ table }: { table: DatatableInstance<D> }) => {
+  const selectedRowsCount = getSelectedRowsCount<D>(table);
+
   return (
-    <thead className="ds-table-header">
+    <thead
+      className="ds-table-header"
+      style={selectedRowsCount > 0 ? { display: 'none' } : null}
+    >
       {table.getHeaderGroups().map((headerGroup) => (
         <HeaderRow
           key={headerGroup.id}
