7b72f43b2a14b7b70eaa18928a67d08dfc4a6aac
Julian Barrionuevo <jbarrionuevo@securityscorecard.io>
2024-03-11 15:40:43 -0300

    fix(DropdownMenu): use radix implementation

--- src/components/_internal/BaseDropdownMenu/DropdownMenu.tsx
+++ src/components/_internal/BaseDropdownMenu/DropdownMenu.tsx
@@ -1,31 +1,13 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { isFunction, isNotUndefined, isNull, noop } from 'ramda-adjunct';
-import cls from 'classnames';
+import { isFunction } from 'ramda-adjunct';
+import * as BaseDropdownMenu from '@radix-ui/react-dropdown-menu';
 
-import {
-  AbsoluteLinkActionKind,
-  ActionKindsPropType,
-  RelativeLinkActionKind,
-} from '../../../types/action.types';
+import { ActionKindsPropType } from '../../../types/action.types';
 import { DropdownLinkProps, DropdownMenuProps } from './DropdownMenu.types';
-import { getColor, getSpace, pxToRem } from '../../../utils';
-import { requireRouterLink } from '../../../utils/require-router-link';
-import { Dropdown } from '../../Dropdown';
-import { SpaceSizes } from '../../../theme/space.enums';
-import { Text, TextEnums } from '../../typographyLegacy';
-import { Padbox, PadboxEnums } from '../../layout';
-import { CLX_COMPONENT } from '../../../theme/constants';
-import { InteractiveElement } from '../../Dropdown/Dropdown.types';
-
-export const List = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  padding-top: ${getSpace(SpaceSizes.sm)};
-  padding-bottom: ${getSpace(SpaceSizes.sm)};
-`;
+import { getColor, pxToRem } from '../../../utils';
+import { Padbox, Stack, Surface } from '../../layout';
 
 const FORWARD_PROP_EXCEPTIONS = ['paddingSize', 'paddingType'];
 
@@ -42,15 +24,11 @@ export const DropdownLink = styled(Padbox).withConfig({
 
   &:hover,
   &:focus-visible {
+    border: 0;
+    outline: 0 !important;
     background: ${getColor('primary.50')};
   }
 `;
-
-const getOptions = () => {
-  return Array.from(
-    document.querySelectorAll<InteractiveElement>('[data-dropdown-item]'),
-  );
-};
 
 const DropdownMenu: React.FC<DropdownMenuProps> = ({
   actions,
@@ -61,135 +39,37 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({
   placement = 'bottom',
 }) => {
   const [isActive, setIsActive] = useState(false);
-  const dropdownRef = useRef({
-    togglePane: noop,
-    hidePane: noop,
-    showPane: noop,
-  });
-  const containerRef = useRef(null);
-  const trigger: React.ReactElement = (
-    <span className={cls(CLX_COMPONENT, className)}>
-      {isFunction(children) ? children(isActive) : children}
-    </span>
-  );
-
-  const handleKeyDown = (event) => {
-    const selectOption = (direction = 'DOWN') => {
-      const options = getOptions();
-      const index = options.indexOf(
-        document.activeElement as InteractiveElement,
-      );
-      const target = options[index + (direction === 'DOWN' ? 1 : -1)];
-      target?.focus();
-    };
-    const fn = {
-      // Focus the next sub-item or item.
-      ArrowDown: () => selectOption('DOWN'),
-      // Focus the previous sub-item or item.
-      ArrowUp: () => selectOption('UP'),
-    }[event.key];
-    fn?.();
-  };
-
-  const handleBlur = () => {
-    setTimeout(() => {
-      const containsActiveElement = containerRef.current?.contains(
-        document.activeElement,
-      );
-      if (!containsActiveElement) {
-        dropdownRef.current?.hidePane();
-      }
-    });
-  };
-
   return (
-    <Dropdown
-      ref={dropdownRef}
-      defaultIsOpen={defaultIsOpen}
-      innerPaddingSize={SpaceSizes.none}
-      maxPaneWidth={paneWidth}
-      placement={placement}
-      trigger={trigger}
-      onClose={() => setIsActive(false)}
-      onOpen={() => setIsActive(true)}
+    <BaseDropdownMenu.Root
+      defaultOpen={defaultIsOpen}
+      onOpenChange={setIsActive}
     >
-      <List ref={containerRef} onBlur={handleBlur} onKeyDown={handleKeyDown}>
-        {actions.map((action) => {
-          let RouterLink = null;
-          if (
-            isNotUndefined(
-              (action as RelativeLinkActionKind<React.MouseEvent[], boolean>)
-                .to,
-            )
-          ) {
-            RouterLink = requireRouterLink();
-          }
-
-          const domTag = isNotUndefined(
-            (action as AbsoluteLinkActionKind<React.MouseEvent[], boolean>)
-              .href,
-          )
-            ? 'a' // render 'a' tag if 'href' is present
-            : isNotUndefined(
-                (action as RelativeLinkActionKind<React.MouseEvent[], boolean>)
-                  .to,
-              )
-            ? RouterLink // render 'Link' if 'to' is present
-            : 'button'; // use default
-
-          if (
-            isNull(RouterLink) &&
-            isNotUndefined(
-              (action as RelativeLinkActionKind<React.MouseEvent[], boolean>)
-                .to,
-            )
-          ) {
-            return null;
-          }
-
-          return (
-            <li
-              key={action.name}
-              data-interactive="true"
-              style={{ overflow: 'hidden' }}
-            >
-              <DropdownLink
-                as={domTag}
-                data-dropdown-item="true"
-                data-interactive="true"
-                href={
-                  (
-                    action as AbsoluteLinkActionKind<
-                      React.MouseEvent[],
-                      boolean
-                    >
-                  ).href
-                }
-                name={action.name}
-                paddingSize={SpaceSizes.md}
-                paddingType={PadboxEnums.PaddingTypes.squish}
-                to={
-                  (
-                    action as RelativeLinkActionKind<
-                      React.MouseEvent[],
-                      boolean
-                    >
-                  ).to
-                }
-                onClick={(event) => {
-                  action.onClick(event);
-                  dropdownRef.current?.hidePane();
-                }}
-              >
-                <Text data-interactive="true" size={TextEnums.TextSizes.md}>
-                  {action.label}
-                </Text>
-              </DropdownLink>
-            </li>
-          );
-        })}
-      </List>
-    </Dropdown>
+      <BaseDropdownMenu.Trigger className={className} asChild>
+        <span>{isFunction(children) ? children(isActive) : children}</span>
+      </BaseDropdownMenu.Trigger>
+      <BaseDropdownMenu.Portal>
+        <BaseDropdownMenu.Content
+          className="DropdownMenuContent"
+          side={placement}
+          sideOffset={5}
+        >
+          <Surface elevation={2} radius="md" hasBorder>
+            <Padbox paddingSize="sm">
+              <Stack gap="sm" style={{ width: paneWidth }}>
+                {actions.map((item) => (
+                  <BaseDropdownMenu.Item
+                    className="DropdownMenuItem"
+                    onSelect={item.onClick}
+                  >
+                    <DropdownLink as="a">{item.label}</DropdownLink>
+                  </BaseDropdownMenu.Item>
+                ))}
+              </Stack>
+            </Padbox>
+          </Surface>
+        </BaseDropdownMenu.Content>
+      </BaseDropdownMenu.Portal>
+    </BaseDropdownMenu.Root>
   );
 };
 
