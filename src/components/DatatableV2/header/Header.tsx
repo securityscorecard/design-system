528e1b9118e14324a5c49de06ac0e41c4c29059b
Radek Podrazky <rpodrazky@securityscorecard.io>
2024-05-10 14:21:02 +0200

    fix(DatatableV2): show header when selection is disabled
    If `enableRowSelection` is set to `false` the selection state can still be used. In this case if selection is triggered from outside the table header will dissapear but the batch row will not be showed. This PR will prevent hiding the table header in this case.

--- src/components/DatatableV2/header/Header.tsx
+++ src/components/DatatableV2/header/Header.tsx
@@ -5,12 +5,17 @@ import HeaderRow from './HeaderRow';
 import { getSelectedRowsCount } from '../toolbar/Selection';
 
 const Header = <D,>({ table }: { table: DatatableInstance<D> }) => {
+  const {
+    options: { enableRowSelection },
+  } = table;
   const selectedRowsCount = getSelectedRowsCount<D>(table);
 
   return (
     <thead
       className="ds-table-header"
-      style={selectedRowsCount > 0 ? { display: 'none' } : null}
+      style={
+        enableRowSelection && selectedRowsCount > 0 ? { display: 'none' } : null
+      }
     >
       {table.getHeaderGroups().map((headerGroup) => (
         <HeaderRow
