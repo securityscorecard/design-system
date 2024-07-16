1560d71ee2f3aa2a67e955c86ebae3439faf6c73
Radek Podrazky <rpodrazky@securityscorecard.io>
2024-07-16 18:38:45 +0200

    build: use Node@22 as build version
    BREAKING CHANGE:
    
    Changing used Node version for building the library should always be treated as breaking.

--- visual-regressions/bin/take-and-check-in-docker-ci.sh
+++ visual-regressions/bin/take-and-check-in-docker-ci.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 radekpodrazky/visual-regressions:node18.20.0-chrome-123.0.6312.86-002 yarn serve-take-and-check
+docker run --rm -v "$PWD/.storybook/image-snapshots/:/usr/src/app/snapshots" -v "$PWD/storybook-static:/usr/src/app/storybook" --platform=linux/amd64 radekpodrazky/visual-regressions:node22.4.1-chrome-126.0.6478.126-001 yarn serve-take-and-check
