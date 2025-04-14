0a1c22edc5b768af47db55f1f6c8bb7d3791f978
Continuous Integration <username@users.noreply.github.com>
2025-04-14 15:56:30 +0000

    Publish docs for 2.18.2

---
+++ 2.18.2/assets/ColumnResizing.stories-DM75wGfN.js
@@ -0,0 +1,26 @@
+import{j as i}from"./jsx-runtime-CfatFE5O.js";import{r as e}from"./index-ClcD9ViR.js";import{D as u}from"./Datatable-DtaDWt6D.js";import{a as S,T as n,c as p}from"./argTypes-C6VlO4PI.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useLogger-DBZ2KDnn.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./PaginationToolbar-Crf4_jmR.js";import"./SelectionToolbar-rqfbzbK3.js";import"./index-U7rWlclI.js";import"./Icon-Bik-9-TI.js";import"./index-DRm3KUNF.js";import"./index-DbwGX_n3.js";import"./index-DxcpycWO.js";import"./index-OY6jNnnn.js";import"./index-DV1EfWHB.js";import"./Padbox-DhupY5rO.js";import"./index-BeSa3Tgc.js";import"./prop-gA1VFyoz.js";import"./Inline-C2UKE3FB.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";import"./index-CEsunubm.js";import"./Button-DXrAv08R.js";import"./components-ByF3bco0.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./useSafeTranslation-DXLRFKFi.js";import"./en-US-CvthOZgl.js";import"./pluck-B5Qa9yNi.js";import"./IconButton-fYiYssr0.js";import"./useContainerQuery-uyIEPHxz.js";import"./Skeleton-DPdGIrHZ.js";import"./useLocalStorageState-UxkHlwiN.js";import"./Paragraph-DWKg1gwn.js";import"./index-CgUcX5gD.js";import"./Tooltip-ClqziCrA.js";import"./FloatingContext-f-xOTKfR.js";import"./index-Cp8NM1Nv.js";import"./CloseButton-DA0CgUBz.js";import"./index-BVsc1NiC.js";import"./isFunction-B5WoXTNq.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./sortable.esm-C-iNasD-.js";import"./modifiers.esm-ByjRBgE8.js";import"./index-xJp9Kd2E.js";import"./index-BiQ22au2.js";import"./Pill-BY7T1u-8.js";import"./PillRemoveButton-BWcc2Zae.js";const jn={title:"components/DatatableV2/ColumnResizing",component:u,parameters:{screenshot:{skip:!0}},argTypes:S},b=n.bind({});b.args=n.args;const f=n.bind({});f.args={...n.args,enableColumnResizing:!1};const h=n.bind({});h.args={...n.args,columns:[...p,{accessorKey:"country",header:"Country",enableResizing:!1}]};const l=n.bind({});l.args={...n.args,defaultColumn:{minSize:200,size:250,maxSize:350}};l.parameters={screenshot:{skip:!1}};const R=n.bind({});R.args={...n.args,columns:[{accessorKey:"country",header:"Country",size:400},...p]};const c=n.bind({});c.args={...n.args,initialState:{columnSizing:{"organization.name":300}}};c.parameters={screenshot:{skip:!1}};const o=g=>{const[r,d]=e.useState({"organization.name":300}),t=e.useRef(null),z=C=>{t.current&&window.cancelAnimationFrame(t.current),t.current=window.requestAnimationFrame(()=>{d(C)})};return i.jsxs(i.Fragment,{children:[i.jsx(u,{state:{columnSizing:r},onColumnSizingChange:z,...g}),i.jsxs("div",{children:[i.jsx("strong",{children:"Debug:"}),i.jsx("pre",{children:JSON.stringify(r,null,2)})]})]})};o.args=n.args;var m,s,a;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`args => {
+  const [columnSizing, setColumnSizing] = useState<ColumnSizingState>({
+    'organization.name': 300
+  });
+  const timeout = useRef<number | null>(null);
+
+  // We recommend to debounce the column resizing due to high performance impact
+  const debouncedSetColumnSizing: OnChangeFn<ColumnSizingState> = params => {
+    if (timeout.current) {
+      window.cancelAnimationFrame(timeout.current);
+    }
+    timeout.current = window.requestAnimationFrame(() => {
+      setColumnSizing(params);
+    });
+  };
+  return <>
+      <Datatable state={{
+      columnSizing
+    }} onColumnSizingChange={debouncedSetColumnSizing} {...args} />
+      <div>
+        <strong>Debug:</strong>
+        <pre>{JSON.stringify(columnSizing, null, 2)}</pre>
+      </div>
+    </>;
+}`,...(a=(s=o.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const En=["ColumnResizingEnabled","DisabledColumnResizing","DisabledColumnResizingForColumn","CustomDefaultColumnsWidth","SpecificSizingForColumn","InitialColumnResizing","ColumnResizingManagedState"];export{b as ColumnResizingEnabled,o as ColumnResizingManagedState,l as CustomDefaultColumnsWidth,f as DisabledColumnResizing,h as DisabledColumnResizingForColumn,c as InitialColumnResizing,R as SpecificSizingForColumn,En as __namedExportsOrder,jn as default};
+//# sourceMappingURL=ColumnResizing.stories-DM75wGfN.js.map
