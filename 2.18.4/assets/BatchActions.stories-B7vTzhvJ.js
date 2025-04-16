f726bec50deec7229f0170d67865aa0fabb7d055
Continuous Integration <username@users.noreply.github.com>
2025-04-16 17:02:39 +0000

    Publish docs for 2.18.4

---
+++ 2.18.4/assets/BatchActions.stories-B7vTzhvJ.js
@@ -0,0 +1,32 @@
+import{j as p}from"./jsx-runtime-CfatFE5O.js";import{B as e}from"./BatchActions-kkTLBD3U.js";import{a as s}from"./actions-ZgHInHH2.js";import{M as a}from"./react-router-dom-CfjGzTlJ.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-BVsc1NiC.js";import"./constants-l-41uwkI.js";import"./index-DRm3KUNF.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./prop-gA1VFyoz.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./index-_M9_Hem9.js";import"./index-B1oE8qgv.js";import"./index-DV1EfWHB.js";import"./Padbox-DhupY5rO.js";import"./index-DxcpycWO.js";import"./index-BJfwPkfN.js";import"./Inline-BnvQG4P8.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";import"./useLogger-DBZ2KDnn.js";import"./index-C_c8NnUg.js";import"./require-router-link-BEFJOFw9.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./index-U7rWlclI.js";import"./Icon-Bik-9-TI.js";import"./index-DbwGX_n3.js";import"./isNull-C_3YYFwP.js";import"./isNotNull-CploqAht.js";import"./noop-CGDyLuY9.js";import"./index-GxL_jJl6.js";import"./DropdownMenu-Diens2qS.js";import"./ControlledDropdown-QQhVOnls.js";import"./usePopper-m9aO9_-k.js";import"./useOuterCallback-D92qb2E8.js";import"./mergeRefs-C43w_hK8.js";import"./FloatingContext-f-xOTKfR.js";import"./Dropdown-BmmuVOH1.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./index-NGm66y6o.js";import"./Datatable.store-q-GLONPa.js";import"./index-CZqIN7mn.js";import"./Tooltip-BV3L2RxT.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./setPrototypeOf-DgZC2w_0.js";import"./tiny-invariant-CopsF_GD.js";const ut={title:"tests/Datatable/BatchModule/BatchActions",component:e,parameters:{viewMode:"story",docs:{disable:!0},previewTabs:{"storybook/docs/panel":{hidden:!0}}},decorators:[o=>p.jsx(a,{children:o()})]},t=o=>p.jsx(e,{...o});t.args={actions:s};t.argTypes={actions:{table:{type:{summary:"Action[]",detail:`
+| {
+    label: string;
+    name: string;
+    onClick: () => void;
+  }
+| {
+    label: string;
+    name: string;
+    to: To;
+    onClick?: () => void;
+    href?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    href: string;
+    onClick?: () => void;
+    to?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    subActions: ActionKinds[];
+    onClick?: () => void;
+    href?: never;
+    to?: never;
+  }
+        `}}}};var r,i,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:"args => <BatchActions {...args} />",...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const gt=["Menu"];export{t as Menu,gt as __namedExportsOrder,ut as default};
+//# sourceMappingURL=BatchActions.stories-B7vTzhvJ.js.map
---
+++ 2.18.4/assets/Pagination.stories-BTh5u4Xh.js
@@ -0,0 +1,35 @@
+import{j as a}from"./jsx-runtime-CfatFE5O.js";import{r as x}from"./index-ClcD9ViR.js";import{u as y,f as w}from"./externalData-CI-40JcC.js";import{D as p}from"./Datatable-BOqY1KeU.js";import{a as k,T as t,c as v}from"./argTypes-CXwkXuzp.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./useLogger-DBZ2KDnn.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./PaginationToolbar-DVRDuBH6.js";import"./SelectionToolbar-CoiqZlXO.js";import"./index-U7rWlclI.js";import"./Icon-Bik-9-TI.js";import"./index-DRm3KUNF.js";import"./index-DbwGX_n3.js";import"./index-DxcpycWO.js";import"./index-B1oE8qgv.js";import"./index-DV1EfWHB.js";import"./Padbox-DhupY5rO.js";import"./index-BJfwPkfN.js";import"./prop-gA1VFyoz.js";import"./Inline-BnvQG4P8.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";import"./index-NGm66y6o.js";import"./Button-DXrAv08R.js";import"./components-ByF3bco0.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./useSafeTranslation-DXLRFKFi.js";import"./en-US-CvthOZgl.js";import"./pluck-B5Qa9yNi.js";import"./IconButton-fYiYssr0.js";import"./useContainerQuery-uyIEPHxz.js";import"./Skeleton-DPdGIrHZ.js";import"./useLocalStorageState-UxkHlwiN.js";import"./Paragraph-DQ9vNjtz.js";import"./index-CZqIN7mn.js";import"./Tooltip-BV3L2RxT.js";import"./FloatingContext-f-xOTKfR.js";import"./index-Cp8NM1Nv.js";import"./CloseButton-DA0CgUBz.js";import"./index-BVsc1NiC.js";import"./isFunction-B5WoXTNq.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./sortable.esm-C-iNasD-.js";import"./modifiers.esm-ByjRBgE8.js";import"./index-xJp9Kd2E.js";import"./index-BEvLCbuv.js";import"./Pill-DF5XtEzd.js";import"./PillRemoveButton-BQqDExGM.js";const qa={title:"components/DatatableV2/Pagination",component:p,parameters:{screenshot:{skip:!0}},argTypes:k},S=t.bind({});S.args=t.args;S.parameters={screenshot:{skip:!1}};const f=t.bind({});f.args={...t.args,enablePagination:!1};f.parameters={screenshot:{skip:!1}};const D=t.bind({});D.args={...t.args,enableRowsPerPage:!0};D.parameters={screenshot:{skip:!1}};const C=t.bind({});C.args={...t.args,rowsPerPageOptions:[5,10,15],enableRowsPerPage:!0};C.parameters={screenshot:{skip:!1}};const j=t.bind({});j.args={...t.args,initialState:{pagination:{pageSize:10,pageIndex:1}}};j.parameters={screenshot:{skip:!1}};const i=o=>{const[e,s]=x.useState({pageIndex:1,pageSize:10});return a.jsxs(a.Fragment,{children:[a.jsx(p,{state:{pagination:e},onPaginationChange:s,...o}),a.jsxs("div",{children:[a.jsx("strong",{children:"Debug:"}),a.jsx("pre",{children:JSON.stringify(e,null,2)})]})]})};i.args=t.args;const r=o=>{var g,m,u;const[e,s]=x.useState({pageIndex:0,pageSize:10}),n=y({queryKey:["data",e],queryFn:()=>w(e),keepPreviousData:!0});return a.jsxs(a.Fragment,{children:[a.jsx(p,{...o,columns:v,data:((g=n==null?void 0:n.data)==null?void 0:g.entries)??[],pageCount:((m=n==null?void 0:n.data)==null?void 0:m.pageCount)??-1,rowCount:(u=n==null?void 0:n.data)==null?void 0:u.rowCount,state:{pagination:e},manualPagination:!0,onPaginationChange:s}),a.jsxs("div",{children:[a.jsx("strong",{children:"Debug:"}),a.jsx("pre",{children:JSON.stringify(e,null,2)})]})]})};var c,d,l;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:`args => {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 1,
+    pageSize: 10
+  });
+  return <>
+      <Datatable state={{
+      pagination
+    }} onPaginationChange={setPagination} {...args} />
+      <div>
+        <strong>Debug:</strong>
+        <pre>{JSON.stringify(pagination, null, 2)}</pre>
+      </div>
+    </>;
+}`,...(l=(d=i.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var P,b,h;r.parameters={...r.parameters,docs:{...(P=r.parameters)==null?void 0:P.docs,source:{originalSource:`args => {
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: 10
+  });
+  const dataQuery = useQuery({
+    queryKey: ['data', pagination],
+    queryFn: () => fetchData(pagination),
+    keepPreviousData: true
+  });
+  return <>
+      <Datatable {...args} columns={columns} data={dataQuery?.data?.entries ?? []} pageCount={dataQuery?.data?.pageCount ?? -1} rowCount={dataQuery?.data?.rowCount} state={{
+      pagination
+    }} manualPagination onPaginationChange={setPagination} />
+      <div>
+        <strong>Debug:</strong>
+        <pre>{JSON.stringify(pagination, null, 2)}</pre>
+      </div>
+    </>;
+}`,...(h=(b=r.parameters)==null?void 0:b.docs)==null?void 0:h.source}}};const Fa=["PaginationEnabled","DisabledPagination","EnabledRowsPerPage","CustomRowsPerPageOptions","InitialPagination","PaginationManagedState","ManualPagination"];export{C as CustomRowsPerPageOptions,f as DisabledPagination,D as EnabledRowsPerPage,j as InitialPagination,r as ManualPagination,S as PaginationEnabled,i as PaginationManagedState,Fa as __namedExportsOrder,qa as default};
+//# sourceMappingURL=Pagination.stories-BTh5u4Xh.js.map
