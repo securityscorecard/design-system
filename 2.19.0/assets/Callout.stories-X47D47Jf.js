e114e42ef0d2181d30c840e86db1a965aaa547d7
Continuous Integration <username@users.noreply.github.com>
2025-05-23 19:39:41 +0000

    Publish docs for 2.19.0

---
+++ 2.19.0/assets/Callout.stories-X47D47Jf.js
@@ -0,0 +1,44 @@
+import{j as r}from"./jsx-runtime-CfatFE5O.js";import{C as e}from"./Callout-C-l2pcza.js";import{L as D}from"./index-BdsenvQQ.js";import{S as y}from"./index-NGm66y6o.js";import"./index-D6ktylzs.js";import{S as z}from"./index-BzJy2gnG.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./constants-l-41uwkI.js";import"./index-Di-8yb3D.js";import"./IconWrapper-BqVDxDe6.js";import"./index-_M9_Hem9.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./Icon-Bik-9-TI.js";import"./index-DRm3KUNF.js";import"./index-DbwGX_n3.js";import"./index-DxcpycWO.js";import"./useLogger-DBZ2KDnn.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./Padbox-Bcaafd_-.js";import"./Inline-CC8LDLht.js";import"./prop-gA1VFyoz.js";import"./index-BiGW80Qt.js";import"./require-router-link-BEFJOFw9.js";import"./react-router-dom-CfjGzTlJ.js";import"./setPrototypeOf-DgZC2w_0.js";import"./tiny-invariant-CopsF_GD.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./isNull-C_3YYFwP.js";import"./isNotNull-CploqAht.js";import"./index-DAJCpEFs.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-MjzvoWTP.js";const pr={title:"components/Callout",component:e,args:{children:r.jsxs(r.Fragment,{children:[r.jsx(y,{children:"Did you know?"})," This is a callout message that may include ",r.jsx(D,{href:"#",children:"links"})," even!"]})}},t={parameters:{screenshot:{skip:!0}}},a={args:{color:"info"}},s={args:{color:"neutral"}},m={render:o=>r.jsxs(z,{gap:"md",children:[r.jsx(e,{...o,icon:{name:"lightbulb",size:"xs"}}),r.jsx(e,{...o,icon:{name:"lightbulb",size:"sm"}}),r.jsx(e,{...o,icon:{name:"lightbulb",size:"md"}})]}),args:{color:"neutral"}},i={render:o=>r.jsxs(z,{gap:"md",children:[r.jsx(e,{...o,color:"info"}),r.jsx(e,{...o,color:"neutral"})]}),parameters:{themes:{themeOverride:"Dark"}}};var n,l,p;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  parameters: {
+    screenshot: {
+      skip: true
+    }
+  }
+}`,...(p=(l=t.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};var c,u,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    color: 'info'
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,h,x;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    color: 'neutral'
+  }
+}`,...(x=(h=s.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var S,k,C;m.parameters={...m.parameters,docs:{...(S=m.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: args => <Stack gap="md">
+      <Callout {...args} icon={{
+      name: 'lightbulb',
+      size: 'xs'
+    }} />
+      <Callout {...args} icon={{
+      name: 'lightbulb',
+      size: 'sm'
+    }} />
+      <Callout {...args} icon={{
+      name: 'lightbulb',
+      size: 'md'
+    }} />
+    </Stack>,
+  args: {
+    color: 'neutral'
+  }
+}`,...(C=(k=m.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};var f,b,j;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <Stack gap="md">
+      <Callout {...args} color="info" />
+      <Callout {...args} color="neutral" />
+    </Stack>,
+  parameters: {
+    themes: {
+      themeOverride: 'Dark'
+    }
+  }
+}`,...(j=(b=i.parameters)==null?void 0:b.docs)==null?void 0:j.source}}};const cr=["Playground","InfoCallout","NeutralCallout","SizedIcon","ColorsDark"];export{i as ColorsDark,a as InfoCallout,s as NeutralCallout,t as Playground,m as SizedIcon,cr as __namedExportsOrder,pr as default};
+//# sourceMappingURL=Callout.stories-X47D47Jf.js.map
---
+++ 2.19.0/assets/SortableList.stories-lPmbJvCb.js
@@ -0,0 +1,20 @@
+import{j as b}from"./jsx-runtime-CfatFE5O.js";import{r as O}from"./index-ClcD9ViR.js";import{a as r}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{S as d}from"./SortableList-CSTHkUZe.js";import{z as h}from"./zipObj-BkK7rA3s.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./v4-CQkTLCs1.js";import"./sortable.esm-C-iNasD-.js";import"./index-_KdZNTQd.js";import"./modifiers.esm-ByjRBgE8.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-D6ktylzs.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./index-DAJCpEFs.js";import"./Padbox-Bcaafd_-.js";import"./index-DxcpycWO.js";import"./index-BzJy2gnG.js";import"./prop-gA1VFyoz.js";import"./Inline-CC8LDLht.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-MjzvoWTP.js";import"./useLogger-DBZ2KDnn.js";import"./DSProvider-BIwlM2Bl.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./index-BVsc1NiC.js";import"./index-DRm3KUNF.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./index-NGm66y6o.js";const et={title:"components/SortableList",component:d},c=["item1","item2","item3","item4","item5","item6"],L=h(c,["Item 1","Item 2","Item 3","Item 4","Item 5","Item 6"]),g=({items:l,...u})=>{const[S,I]=O.useState(l);return b.jsx(d,{...u,items:S,onOrderChange:o=>{r("onOrderChange")(o),I(o)}})},t=g.bind({});t.args={items:c,labels:L,onDragStart:r("onDragStart"),onDragOver:r("onDragOver"),onDragEnd:r("onDragEnd"),onDragCancel:r("onDragCancel")};t.parameters={screenshot:{skip:!0}};const e=g.bind({});e.args={...t.args,labels:void 0};var s,m,a;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`({
+  items,
+  ...args
+}) => {
+  const [sortedItems, setSortedItems] = useState(items);
+  return <SortableList {...args} items={sortedItems} onOrderChange={keys => {
+    action('onOrderChange')(keys);
+    setSortedItems(keys);
+  }} />;
+}`,...(a=(m=t.parameters)==null?void 0:m.docs)==null?void 0:a.source}}};var i,n,p;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`({
+  items,
+  ...args
+}) => {
+  const [sortedItems, setSortedItems] = useState(items);
+  return <SortableList {...args} items={sortedItems} onOrderChange={keys => {
+    action('onOrderChange')(keys);
+    setSortedItems(keys);
+  }} />;
+}`,...(p=(n=e.parameters)==null?void 0:n.docs)==null?void 0:p.source}}};const ot=["Playground","WithoutLabels"];export{t as Playground,e as WithoutLabels,ot as __namedExportsOrder,et as default};
+//# sourceMappingURL=SortableList.stories-lPmbJvCb.js.map
