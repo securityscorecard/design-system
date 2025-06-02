1435b0f4f1d8b433db40282bd7f428865b94dba1
Continuous Integration <username@users.noreply.github.com>
2025-06-02 16:52:55 +0000

    Publish docs for 2.20.3

---
+++ 2.20.3/assets/BatchActions.stories-CfPcufSG.js
@@ -0,0 +1,32 @@
+import{j as p}from"./jsx-runtime-CfatFE5O.js";import{B as e}from"./BatchActions-DU_qMn_1.js";import{a as s}from"./actions-uEjqy8Lr.js";import{M as a}from"./react-router-dom-CfjGzTlJ.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-BVsc1NiC.js";import"./constants-l-41uwkI.js";import"./index-DRm3KUNF.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./prop-gA1VFyoz.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./index-_M9_Hem9.js";import"./index-BH46DRTB.js";import"./index-Bi90Y8Ou.js";import"./Padbox-D0nj4Q3C.js";import"./index-DxcpycWO.js";import"./index-BeSa3Tgc.js";import"./Inline-CzyJe68E.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";import"./useLogger-DBZ2KDnn.js";import"./index-Dzby0U3-.js";import"./require-router-link-BEFJOFw9.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./index-U7rWlclI.js";import"./Icon-Bik-9-TI.js";import"./index-DbwGX_n3.js";import"./isNull-C_3YYFwP.js";import"./isNotNull-CploqAht.js";import"./noop-CGDyLuY9.js";import"./index-D2Wgz3kD.js";import"./DropdownMenu-Ch-j-8kP.js";import"./ControlledDropdown-CEQYqVo0.js";import"./usePopper-m9aO9_-k.js";import"./useOuterCallback-D92qb2E8.js";import"./mergeRefs-C43w_hK8.js";import"./FloatingContext-f-xOTKfR.js";import"./Dropdown-Bdd-eIJT.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./index-CEsunubm.js";import"./Datatable.store-q-GLONPa.js";import"./index-DUH0Ap2M.js";import"./Tooltip-Csv8ziFU.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./setPrototypeOf-DgZC2w_0.js";import"./tiny-invariant-CopsF_GD.js";const ut={title:"tests/Datatable/BatchModule/BatchActions",component:e,parameters:{viewMode:"story",docs:{disable:!0},previewTabs:{"storybook/docs/panel":{hidden:!0}}},decorators:[o=>p.jsx(a,{children:o()})]},t=o=>p.jsx(e,{...o});t.args={actions:s};t.argTypes={actions:{table:{type:{summary:"Action[]",detail:`
+| {
+    label: string;
+    name: string;
+    onClick: () => void;
+  }
+| {
+    label: string;
+    name: string;
+    to: To;
+    onClick?: () => void;
+    href?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    href: string;
+    onClick?: () => void;
+    to?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    subActions: ActionKinds[];
+    onClick?: () => void;
+    href?: never;
+    to?: never;
+  }
+        `}}}};var r,i,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:"args => <BatchActions {...args} />",...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const gt=["Menu"];export{t as Menu,gt as __namedExportsOrder,ut as default};
+//# sourceMappingURL=BatchActions.stories-CfPcufSG.js.map
---
+++ 2.20.3/assets/lt-CVWeHBEq.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./_isObject-dmcfxmtS.js";var n=o(function(r,t){return r<t});export{n as l};
+//# sourceMappingURL=lt-CVWeHBEq.js.map
