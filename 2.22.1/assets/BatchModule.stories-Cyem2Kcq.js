97bd98d642a6b3adeefaa92fd2d779300019e4f8
Continuous Integration <username@users.noreply.github.com>
2025-06-19 13:38:41 +0000

    Publish docs for 2.22.1

---
+++ 2.22.1/assets/BatchModule.stories-Cyem2Kcq.js
@@ -0,0 +1,54 @@
+import{j as e}from"./jsx-runtime-CfatFE5O.js";import{r as E}from"./index-ClcD9ViR.js";import{a as j}from"./actions-D69e5ugp.js";import{B as i}from"./BatchModule-3nBrNMVo.js";import{D as n}from"./Datatable.store-q-GLONPa.js";import{d as c}from"./defaultConfigs-BamTNqs9.js";import{M as I}from"./react-router-dom-J82xx1I9.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./Inline-C2UKE3FB.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-DxcpycWO.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./prop-gA1VFyoz.js";import"./Icon-Bik-9-TI.js";import"./index-DRm3KUNF.js";import"./index-DbwGX_n3.js";import"./useLogger-DBZ2KDnn.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./index-CgUcX5gD.js";import"./Tooltip-ClqziCrA.js";import"./index-OY6jNnnn.js";import"./index-DV1EfWHB.js";import"./Padbox-DhupY5rO.js";import"./index-BeSa3Tgc.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";import"./FloatingContext-f-xOTKfR.js";import"./index-BAutkprd.js";import"./ColumnsControls-U_1ZRvc2.js";import"./ControlDropdown-BQNe_O9d.js";import"./index-BVsc1NiC.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./Button-DXrAv08R.js";import"./components-ByF3bco0.js";import"./index-U7rWlclI.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./index-Cp8NM1Nv.js";import"./CloseButton-DA0CgUBz.js";import"./IconButton-fYiYssr0.js";import"./index-BBJ1g9mG.js";import"./Heading-Wkmas1ro.js";import"./ControlledDropdown-DOMO0UHv.js";import"./usePopper-m9aO9_-k.js";import"./useOuterCallback-D92qb2E8.js";import"./noop-CGDyLuY9.js";import"./mergeRefs-C43w_hK8.js";import"./pick-DLB1qMU0.js";import"./SortableList-DCv_g4LB.js";import"./sortable.esm-C-iNasD-.js";import"./modifiers.esm-ByjRBgE8.js";import"./index-CEsunubm.js";import"./lt-CVWeHBEq.js";import"./index-CyZxMOeP.js";import"./MultiValueInput-BeuRC0f7.js";import"./index-BiQ22au2.js";import"./Pill-BY7T1u-8.js";import"./PillRemoveButton-BWcc2Zae.js";import"./isNonEmptyArray-Do-5vzXl.js";import"./isNotEmpty-DRLlCpEq.js";import"./isNonEmptyString-CZs63_i6.js";import"./isNotNull-CploqAht.js";import"./isNull-C_3YYFwP.js";import"./trim-39gE4VMH.js";import"./take-BBQ9_XYD.js";import"./index-Cwjnxdqs.js";import"./index-D-6HVrdk.js";import"./Checkbox-xfOTOUl8.js";import"./index-zG1Ob1bl.js";import"./Label-DV_70g_N.js";import"./ElementLabel-Kzoagp5M.js";import"./index-poFWmYtr.js";import"./Input-BNqQr89a.js";import"./Message-mRbMqLBk.js";import"./Paragraph-DWKg1gwn.js";import"./Password-CIkJB-4O.js";import"./Radio-SgxJw5qv.js";import"./index-Dn144Sxx.js";import"./Select-wZCp6nVG.js";import"./useSelectProps-BeAuURKk.js";import"./unsupportedIterableToArray-xuqNRLo1.js";import"./setPrototypeOf-DgZC2w_0.js";import"./isNotNilOrEmpty-CTx4I37N.js";import"./isNilOrEmpty-CZt-ddG5.js";import"./isNil-D8uoaQyf.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./sortBy-BJfiWH6l.js";import"./CreatableSelect-YU3h_j4m.js";import"./Range-DSLkGWsF.js";import"./Switch-Ce72L8IK.js";import"./subtract-C44IRfUK.js";import"./TextArea-_SR7gJVt.js";import"./SearchBar-Cja3oQ6E.js";import"./SegmentedToggle-C29ZuXIg.js";import"./ControlButton-KBOpIlfa.js";import"./Chip-ZSC_PkwE.js";import"./utils-DUvRoR_o.js";import"./ChipLabel-D5nrfaES.js";import"./BatchActions-DCF5ZJgG.js";import"./index-D-Wvu7Mc.js";import"./require-router-link-CWq1HiHm.js";import"./index-BdpHVcfx.js";import"./DropdownMenu-coVQdJdO.js";import"./index-DFbU2e9M.js";import"./Dropdown-BJ6XuWM6.js";import"./inheritsLoose-B6jfs0L0.js";import"./ElementCounter-CiitGdtm.js";import"./all-DUu1LMMi.js";import"./index-BKAvfap-.js";import"./BaseTable.styles-BCByPnF0.js";import"./index-BiGW80Qt.js";import"./flip-BOEdXAWn.js";import"./Footer-DL230eVU.js";import"./Pagination-4bY1AWTN.js";import"./tiny-invariant-CopsF_GD.js";const Nr={title:"tests/Datatable/BatchModule",component:i,decorators:[t=>e.jsx(I,{children:t()})],parameters:{viewMode:"story"},argTypes:{actions:{table:{defaultValue:{summary:"[]"},type:{summary:"Action[]",detail:`| {
+    label: string;
+    name: string;
+    onClick: () => void;
+  }
+| {
+    label: string;
+    name: string;
+    to: To;
+    onClick?: () => void;
+    href?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    href: string;
+    onClick?: () => void;
+    to?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    subActions: ActionKinds[];
+    onClick?: () => void;
+    href?: never;
+    to?: never;
+  }`}}},shouldShowSelectionDropdown:{control:{type:"boolean"},table:{defaultValue:{summary:c.hasSelection.toString()}}}}},r=t=>e.jsx(i,{...t});r.args={actions:[],dataSize:107e4,hasSelection:c.hasSelection,hasOnlyPerPageSelection:c.hasOnlyPerPageSelection,columns:{isButtonDisplayed:!1}};r.parameters={screenshot:{skip:!0}};const s=t=>(E.useEffect(()=>(n.update(o=>{o.selectedIds=["a","b","c","d","e","f"]}),()=>{n.update(o=>{o.selectedIds=[]})}),[]),e.jsx(i,{...t}));s.args=r.args;const m=t=>(E.useEffect(()=>(n.update(o=>{o.selectedIds=["a","b","c","d","e","f"]}),()=>{n.update(o=>{o.selectedIds=[]})}),[]),e.jsx(i,{...t}));m.args={...r.args,hasOnlyPerPageSelection:!1};const p=t=>e.jsx(i,{...t});p.args={...r.args,actions:j};const a=t=>e.jsx(i,{...t});a.args={...r.args,columns:{isButtonDisplayed:!0}};var d,l,u;r.parameters={...r.parameters,docs:{...(d=r.parameters)==null?void 0:d.docs,source:{originalSource:"args => <BatchModule {...args} />",...(u=(l=r.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var g,h,f;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`args => {
+  useEffect(() => {
+    DatatableStore.update(s => {
+      s.selectedIds = ['a', 'b', 'c', 'd', 'e', 'f'];
+    });
+    return () => {
+      DatatableStore.update(s => {
+        s.selectedIds = [];
+      });
+    };
+  }, []);
+  return <BatchModule {...args} />;
+}`,...(f=(h=s.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};var S,b,y;m.parameters={...m.parameters,docs:{...(S=m.parameters)==null?void 0:S.docs,source:{originalSource:`args => {
+  useEffect(() => {
+    DatatableStore.update(s => {
+      s.selectedIds = ['a', 'b', 'c', 'd', 'e', 'f'];
+    });
+    return () => {
+      DatatableStore.update(s => {
+        s.selectedIds = [];
+      });
+    };
+  }, []);
+  return <BatchModule {...args} />;
+}`,...(y=(b=m.parameters)==null?void 0:b.docs)==null?void 0:y.source}}};var x,B,M;p.parameters={...p.parameters,docs:{...(x=p.parameters)==null?void 0:x.docs,source:{originalSource:"args => <BatchModule {...args} />",...(M=(B=p.parameters)==null?void 0:B.docs)==null?void 0:M.source}}};var v,D,A;a.parameters={...a.parameters,docs:{...(v=a.parameters)==null?void 0:v.docs,source:{originalSource:"args => <BatchModule {...args} />",...(A=(D=a.parameters)==null?void 0:D.docs)==null?void 0:A.source}}};const Qr=["Playground","WithSelectedRows","WithSelectedRowsAndExclusionLogic","WithBatchActions","WithColumns"];export{r as Playground,p as WithBatchActions,a as WithColumns,s as WithSelectedRows,m as WithSelectedRowsAndExclusionLogic,Qr as __namedExportsOrder,Nr as default};
+//# sourceMappingURL=BatchModule.stories-Cyem2Kcq.js.map
---
+++ 2.22.1/assets/Wizard.stories-B1R_JYV2.js
@@ -0,0 +1,26 @@
+import{j as e}from"./jsx-runtime-CfatFE5O.js";import{a as b}from"./chunk-D5ZWXAHU-CGElDDNX.js";import{W as n,a as r}from"./WizardStep-D2q-XN5D.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./v4-CQkTLCs1.js";import"./index-OY6jNnnn.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./index-DV1EfWHB.js";import"./Padbox-DhupY5rO.js";import"./index-DxcpycWO.js";import"./index-BeSa3Tgc.js";import"./prop-gA1VFyoz.js";import"./Inline-C2UKE3FB.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";import"./useLogger-DBZ2KDnn.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./index-B2ekhNDD.js";import"./Modal-C95r0t6D.js";import"./useOuterCallback-D92qb2E8.js";import"./index-dIq9_GSP.js";import"./index-BBJ1g9mG.js";import"./Heading-Wkmas1ro.js";import"./mergeRefs-C43w_hK8.js";import"./index-BVsc1NiC.js";import"./index-DRm3KUNF.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./index-Cp8NM1Nv.js";import"./CloseButton-DA0CgUBz.js";import"./IconButton-fYiYssr0.js";import"./components-ByF3bco0.js";import"./index-U7rWlclI.js";import"./Icon-Bik-9-TI.js";import"./index-DbwGX_n3.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./FloatingContext-f-xOTKfR.js";import"./Button-DXrAv08R.js";import"./Step-DrJu77-j.js";import"./useContainerQuery-uyIEPHxz.js";import"./index-CEsunubm.js";const be={title:"components/Wizard",component:n,subcomponents:{WizardStep:r},parameters:{docs:{story:{inline:!1,height:800},source:{type:"code"}}},tags:["deprecated"]},m=({isBackwardNavigationEnabled:o=!1,initialStep:k=void 0})=>e.jsxs(n,{initialStep:k,isBackwardNavigationEnabled:o,onStepChange:b("Wizard/on-step-change"),children:[e.jsx(r,{id:"step-one",name:"Step One",primaryAction:{label:"Next Step",onClick:({goToNextStep:t})=>t()},children:"Step one"}),e.jsx(r,{id:"step-two",name:"Step Two",primaryAction:{label:"Next",onClick:({goToNextStep:t})=>t()},secondaryAction:{label:"Previous",onClick:({goToPreviousStep:t})=>t()},children:"Step Two"}),e.jsx(r,{id:"step-three",name:"Step Three",primaryAction:{label:"Submit",onClick:({goToNextStep:t})=>t()},secondaryAction:{label:"Previous",onClick:({goToPreviousStep:t})=>t()},children:"Step Three"})]}),i=()=>e.jsx(m,{}),p=()=>e.jsxs(n,{onStepChange:b("Wizard/on-step-change"),children:[e.jsx(r,{id:"step-one",name:"Step One",primaryAction:{label:"Next Step",onClick:({goToNextStep:o})=>o()},children:"Step one"}),e.jsx(r,{id:"step-two",name:"Step Two",primaryAction:{label:"Next",onClick:({goToNextStep:o})=>o()},secondaryAction:{label:"Previous",onClick:({goToPreviousStep:o})=>o()},children:"Step Two"})]}),a=()=>e.jsx(m,{isBackwardNavigationEnabled:!0}),s=()=>e.jsx(m,{initialStep:"step-two"});var c,d,l;i.parameters={...i.parameters,docs:{...(c=i.parameters)==null?void 0:c.docs,source:{originalSource:"() => <WizardTemplate />",...(l=(d=i.parameters)==null?void 0:d.docs)==null?void 0:l.source}}};var S,u,g;p.parameters={...p.parameters,docs:{...(S=p.parameters)==null?void 0:S.docs,source:{originalSource:`() => {
+  return <Wizard onStepChange={action('Wizard/on-step-change')}>
+      <WizardStep id="step-one" name="Step One" primaryAction={{
+      label: 'Next Step',
+      onClick: ({
+        goToNextStep
+      }) => goToNextStep()
+    }}>
+        Step one
+      </WizardStep>
+      <WizardStep id="step-two" name="Step Two" primaryAction={{
+      label: 'Next',
+      onClick: ({
+        goToNextStep
+      }) => goToNextStep()
+    }} secondaryAction={{
+      label: 'Previous',
+      onClick: ({
+        goToPreviousStep
+      }) => goToPreviousStep()
+    }}>
+        Step Two
+      </WizardStep>
+    </Wizard>;
+}`,...(g=(u=p.parameters)==null?void 0:u.docs)==null?void 0:g.source}}};var x,W,h;a.parameters={...a.parameters,docs:{...(x=a.parameters)==null?void 0:x.docs,source:{originalSource:"() => <WizardTemplate isBackwardNavigationEnabled />",...(h=(W=a.parameters)==null?void 0:W.docs)==null?void 0:h.source}}};var T,z,w;s.parameters={...s.parameters,docs:{...(T=s.parameters)==null?void 0:T.docs,source:{originalSource:'() => <WizardTemplate initialStep="step-two" />',...(w=(z=s.parameters)==null?void 0:z.docs)==null?void 0:w.source}}};const ke=["Playground","TwoStepWizard","WithBackwardsNavigation","WithInitialStep"];export{i as Playground,p as TwoStepWizard,a as WithBackwardsNavigation,s as WithInitialStep,ke as __namedExportsOrder,be as default};
+//# sourceMappingURL=Wizard.stories-B1R_JYV2.js.map
