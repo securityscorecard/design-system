64d0418745f506b1e0e18bbaa769b547970b91ef
Continuous Integration <username@users.noreply.github.com>
2025-07-16 03:33:47 +0000

    Publish docs for 2.23.2

---
+++ 2.23.2/assets/Grid.stories-Ba3MfVvh.js
@@ -0,0 +1,53 @@
+import{j as o}from"./jsx-runtime-CfatFE5O.js";import{S as L}from"./space.enums-DGrZwzIk.js";import{B as e}from"./Box-V1rRRriq.js";import{G as x}from"./index-DCGZYvlw.js";import"./index-DM9Ox8Ke.js";import{T as v}from"./index-CEsunubm.js";import{C as S,a as W,b as w}from"./CardMedia-DX6jbySN.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./_isObject-dmcfxmtS.js";import"./index-DAJCpEFs.js";import"./Padbox-Bcaafd_-.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-DxcpycWO.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./index-BXr0x_2P.js";import"./prop-gA1VFyoz.js";import"./Inline-mI01sxMS.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-MjzvoWTP.js";import"./useLogger-DBZ2KDnn.js";import"./DSProvider-BIwlM2Bl.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./index-BVsc1NiC.js";import"./index-DRm3KUNF.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./index-OqTTQXUe.js";import"./index-mM0lCJqK.js";import"./require-router-link-CWq1HiHm.js";import"./react-router-dom-J82xx1I9.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-DbwGX_n3.js";import"./tiny-invariant-CopsF_GD.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./index-U7rWlclI.js";import"./Icon-Bik-9-TI.js";import"./isNull-C_3YYFwP.js";import"./isNotNull-CploqAht.js";import"./noop-CGDyLuY9.js";import"./Button-BK2_KQbU.js";import"./isNonEmptyString-CZs63_i6.js";import"./isNotEmpty-DRLlCpEq.js";import"./index-Br_kR6IH.js";import"./DropdownMenu-O3hcZCRU.js";import"./index-Ctz4h7vk.js";import"./ControlledDropdown-DwlPSVyU.js";import"./usePopper-m9aO9_-k.js";import"./useOuterCallback-D92qb2E8.js";import"./mergeRefs-C43w_hK8.js";import"./FloatingContext-f-xOTKfR.js";import"./Dropdown-Z80-W2oW.js";import"./index-BBJ1g9mG.js";import"./Heading-Wkmas1ro.js";import"./IconButton-fYiYssr0.js";import"./components-ByF3bco0.js";import"./index-Y666a7qX.js";import"./HintTooltip-BhuyeV8N.js";import"./index-sYUuX_NM.js";import"./Tooltip-C-Mt1qrC.js";import"./isNil-D8uoaQyf.js";const Ko={title:"layout/primitives/Grid",component:x,argTypes:{align:{options:["center","flex-end","flex-start","baseline","stretch"],control:{type:"select"},table:{defaultValue:{summary:'"stretch"'}}},wrapperEl:{control:{type:"text"},table:{defaultValue:{summary:'"div"'}}},parentEl:{control:{type:"text"},table:{defaultValue:{summary:'"div"'}}},wrapperOverflow:{options:["hidden","visible"],control:{type:"radio"},table:{defaultValue:{summary:'"hidden"'}}},cols:{control:{type:"number",min:2}}}},a=p=>o.jsxs(x,{...p,children:[o.jsx(e,{children:"Item"}),o.jsx(e,{children:"Long item"}),o.jsx(e,{children:"Very long item item item"}),o.jsx(e,{children:"Item"}),o.jsx(e,{children:"Item"}),o.jsx(e,{children:"Long item"}),o.jsx(e,{children:"Very long item item item item"}),o.jsx(e,{children:"Very long item item item"}),o.jsx(e,{children:"Item"}),o.jsx(e,{children:"Long item"})]}),i=a.bind({});i.args={gap:"md"};i.parameters={screenshot:{skip:!0}};const t=a.bind({});t.args={gap:L.sm};const m=a.bind({});m.args={...t.args,align:"center"};const s=a.bind({});s.args={...t.args,cols:3};const r=()=>o.jsxs(S,{children:[o.jsx(W,{subtitle:"Card subtitle",title:"Card title"}),o.jsx(w,{children:o.jsx(v,{size:"md",children:"Etiam id laoreet tellus. Pellentesque interdum porttitor iaculis. Ut leo urna, lobortis ac fermentum at, efficitur vel lorem. Cras viverra tempor augue, sed rutrum neque vestibulum at."})})]}),n=p=>o.jsxs(x,{...p,children:[o.jsx(r,{}),o.jsx(r,{}),o.jsx(r,{}),o.jsx(r,{}),o.jsx(r,{}),o.jsx(r,{})]});n.args={gap:L.lg,cols:3,wrapperOverflow:"visible"};var l,d,B;i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`args => <Grid {...args}>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+  </Grid>`,...(B=(d=i.parameters)==null?void 0:d.docs)==null?void 0:B.source}}};var c,g,u;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`args => <Grid {...args}>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+  </Grid>`,...(u=(g=t.parameters)==null?void 0:g.docs)==null?void 0:u.source}}};var h,y,C;m.parameters={...m.parameters,docs:{...(h=m.parameters)==null?void 0:h.docs,source:{originalSource:`args => <Grid {...args}>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+  </Grid>`,...(C=(y=m.parameters)==null?void 0:y.docs)==null?void 0:C.source}}};var j,V,b;s.parameters={...s.parameters,docs:{...(j=s.parameters)==null?void 0:j.docs,source:{originalSource:`args => <Grid {...args}>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+    <Box>Very long item item item item</Box>
+    <Box>Very long item item item</Box>
+    <Box>Item</Box>
+    <Box>Long item</Box>
+  </Grid>`,...(b=(V=s.parameters)==null?void 0:V.docs)==null?void 0:b.source}}};var f,I,G;n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`args => <Grid {...args}>
+    <ComposedCard />
+    <ComposedCard />
+    <ComposedCard />
+    <ComposedCard />
+    <ComposedCard />
+    <ComposedCard />
+  </Grid>`,...(G=(I=n.parameters)==null?void 0:I.docs)==null?void 0:G.source}}};const Mo=["Playground","WithGap","WithVerticalAlignment","WithCustomColumnsNumber","WithVisibleOverflow"];export{i as Playground,s as WithCustomColumnsNumber,t as WithGap,m as WithVerticalAlignment,n as WithVisibleOverflow,Mo as __namedExportsOrder,Ko as default};
+//# sourceMappingURL=Grid.stories-Ba3MfVvh.js.map
