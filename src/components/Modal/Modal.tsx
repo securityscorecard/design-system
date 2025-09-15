578c87495c3409c45ee5094b80fc7e6d3e844850
Loengry <loengry.sequeira@securityscorecard.io>
2025-09-14 20:24:26 -0600

    feat(focustrap): support for modal and drawer. PLAT-928

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -18,6 +18,7 @@ import { CloseButton } from '../CloseButton';
 import { StretchEnum } from '../layout/Inline/Inline.enums';
 import { CLX_COMPONENT } from '../../theme/constants';
 import { FloatingProvider } from '../../contexts/FloatingContext';
+import FocusTrap from '../FocusTrap';
 
 const widthVariants = {
   [ModalSizes.xs]: 320,
@@ -82,32 +83,34 @@ const Modal = forwardRef<HTMLDivElement, ModalProps>(
       <FloatingProvider>
         <Portal>
           <Overlay data-testid="dialog-overlay" placement="center">
-            <BaseModal
-              ref={mergeRefs<HTMLDivElement>(modalRef, ref)}
-              $maxWidth={widthVariants[size]}
-              className={cls(CLX_COMPONENT, className, 'ssc-ui-styled')}
-              elevation={3}
-              radius="lg"
-              hasBorder
-              {...rest}
-            >
-              <Inline stretch={StretchEnum.start}>
-                <Header className="sscds-modal-header">
-                  {typeof title !== 'undefined' && <Title>{title}</Title>}
-                </Header>
-                {onClose && (
-                  <CloseButton
-                    marginCompensation={SpaceSizes.none}
-                    style={{ position: 'relative', zIndex: 1 }}
-                    onClose={onClose}
-                  />
+            <FocusTrap isActive>
+              <BaseModal
+                ref={mergeRefs<HTMLDivElement>(modalRef, ref)}
+                $maxWidth={widthVariants[size]}
+                className={cls(CLX_COMPONENT, className, 'ssc-ui-styled')}
+                elevation={3}
+                radius="lg"
+                hasBorder
+                {...rest}
+              >
+                <Inline stretch={StretchEnum.start}>
+                  <Header className="sscds-modal-header">
+                    {typeof title !== 'undefined' && <Title>{title}</Title>}
+                  </Header>
+                  {onClose && (
+                    <CloseButton
+                      marginCompensation={SpaceSizes.none}
+                      style={{ position: 'relative', zIndex: 1 }}
+                      onClose={onClose}
+                    />
+                  )}
+                </Inline>
+                <Content className="sscds-modal-content">{children}</Content>
+                {typeof footer !== 'undefined' && (
+                  <Footer className="sscds-modal-footer">{footer}</Footer>
                 )}
-              </Inline>
-              <Content className="sscds-modal-content">{children}</Content>
-              {typeof footer !== 'undefined' && (
-                <Footer className="sscds-modal-footer">{footer}</Footer>
-              )}
-            </BaseModal>
+              </BaseModal>
+            </FocusTrap>
           </Overlay>
         </Portal>
       </FloatingProvider>
