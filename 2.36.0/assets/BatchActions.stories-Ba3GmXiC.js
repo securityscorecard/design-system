cf31e31d83d7a7a5a65779ba6a61735ffa02ad99
Continuous Integration <username@users.noreply.github.com>
2025-09-17 09:39:02 +0000

    Publish docs for 2.36.0

---
+++ 2.36.0/assets/BatchActions.stories-Ba3GmXiC.js
@@ -0,0 +1,32 @@
+import{j as p}from"./jsx-runtime-CfatFE5O.js";import{B as e}from"./BatchActions-ClADARsQ.js";import{a as s}from"./actions-C5K4rShA.js";import{M as a}from"./react-router-dom-BYZ7Fv1B.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-CRdrLk6A.js";import"./constants-l-41uwkI.js";import"./index-DRm3KUNF.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./DSProvider-DJmllnwz.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./isFunction-B5WoXTNq.js";import"./pluck-B5Qa9yNi.js";import"./prop-gA1VFyoz.js";import"./minus-C2f59QhV.js";import"./checkCircleSolid-CXNAtMl8.js";import"./index-_M9_Hem9.js";import"./index-C7a4l8Yi.js";import"./index-RI_itpZj.js";import"./Padbox-CyAbkeyF.js";import"./index-DxcpycWO.js";import"./index-BzJy2gnG.js";import"./Inline-BZCq97yM.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-MjzvoWTP.js";import"./useLogger-DB992SI6.js";import"./index-CO-W_8NJ.js";import"./require-router-link-C94ZReSM.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./index-rImj20zY.js";import"./Icon-BBciP95r.js";import"./index-DbwGX_n3.js";import"./isNull-C_3YYFwP.js";import"./isNotNull-CploqAht.js";import"./noop-CGDyLuY9.js";import"./index-BI9Reg7s.js";import"./DropdownMenu-DgQc43YG.js";import"./index-DBFjSybC.js";import"./ControlledDropdown-CMFxWhqj.js";import"./usePopper-m9aO9_-k.js";import"./useOuterCallback-BFSBiNPT.js";import"./mergeRefs-C43w_hK8.js";import"./FloatingContext-f-xOTKfR.js";import"./Dropdown-BaNMoH-t.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./index-CEsunubm.js";import"./Datatable.store-q-GLONPa.js";import"./index-CLFfF-QT.js";import"./Tooltip-BjqDIMiY.js";import"./chunk-D5ZWXAHU-CGElDDNX.js";import"./v4-CQkTLCs1.js";import"./tiny-invariant-CopsF_GD.js";const bt={title:"tests/Datatable/BatchModule/BatchActions",component:e,parameters:{viewMode:"story",docs:{disable:!0},previewTabs:{"storybook/docs/panel":{hidden:!0}}},decorators:[o=>p.jsx(a,{children:o()})]},t=o=>p.jsx(e,{...o});t.args={actions:s};t.argTypes={actions:{table:{type:{summary:"Action[]",detail:`
+| {
+    label: string;
+    name: string;
+    onClick: () => void;
+  }
+| {
+    label: string;
+    name: string;
+    to: To;
+    onClick?: () => void;
+    href?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    href: string;
+    onClick?: () => void;
+    to?: never;
+    subActions?: never;
+  }
+| {
+    label: string;
+    name: string;
+    subActions: ActionKinds[];
+    onClick?: () => void;
+    href?: never;
+    to?: never;
+  }
+        `}}}};var r,i,m;t.parameters={...t.parameters,docs:{...(r=t.parameters)==null?void 0:r.docs,source:{originalSource:"args => <BatchActions {...args} />",...(m=(i=t.parameters)==null?void 0:i.docs)==null?void 0:m.source}}};const vt=["Menu"];export{t as Menu,vt as __namedExportsOrder,bt as default};
+//# sourceMappingURL=BatchActions.stories-Ba3GmXiC.js.map
---
+++ 2.36.0/assets/CompanyLogo-Bb3X1kEb.js
@@ -0,0 +1,27 @@
+import{j as o}from"./jsx-runtime-CfatFE5O.js";import{useMDXComponents as s}from"./index-DUy19JZU.js";import{ae as t,ag as a,af as i,ao as l}from"./index-axE874Ru.js";import{C as p,D as d,S as c}from"./CompanyLogo.stories-Cw1GW2sP.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-BCP1Z7dD.js";import"../sb-preview/runtime.js";import"./index-_KdZNTQd.js";import"./index-CJyPbrN5.js";import"./index-DrFu-skq.js";import"./decorators-uTMKE_pn.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./index-DGqv0G-5.js";import"./Spinner-CK2vC9iP.js";import"./index-DxcpycWO.js";import"./index-_M9_Hem9.js";import"./constants-l-41uwkI.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./index-rImj20zY.js";import"./Icon-BBciP95r.js";import"./index-DRm3KUNF.js";import"./index-DbwGX_n3.js";import"./useLogger-DB992SI6.js";import"./DSProvider-DJmllnwz.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./Inline-BZCq97yM.js";import"./prop-gA1VFyoz.js";function r(n){const e={code:"code",h1:"h1",h2:"h2",h3:"h3",p:"p",pre:"pre",...s(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(t,{of:p}),`
+`,o.jsx(e.h1,{id:"companylogo",children:"CompanyLogo"}),`
+`,o.jsx(a,{children:o.jsx(e.p,{children:`A versatile container designed for the consistent display of company logos. It
+ensures that logos are presented in an uniform manner across different parts
+of an application.`})}),`
+`,o.jsx(e.h2,{id:"purpose",children:"Purpose"}),`
+`,o.jsxs(e.p,{children:["The primary purpose of the ",o.jsx(e.code,{children:"CompanyLogo"})," component is to provide a standardized way to display company logos, handling various states such as loading, error, and success. It ensures that logos are displayed with the correct aspect ratio, size, and styling, maintaining a cohesive look and feel."]}),`
+`,o.jsx(e.h2,{id:"usage",children:"Usage"}),`
+`,o.jsxs(e.p,{children:["To use the ",o.jsx(e.code,{children:"CompanyLogo"})," component, simply import it:"]}),`
+`,o.jsx(e.pre,{children:o.jsx(e.code,{className:"language-js",children:`import { CompanyLogo } from '@security-scorecard/design-system';
+`})}),`
+`,o.jsxs(e.p,{children:["Then provide the required props ",o.jsx(e.code,{children:"src"})," for the logo URL, ",o.jsx(e.code,{children:"alt"})," for alternative text."]}),`
+`,o.jsx(e.p,{children:"Example:"}),`
+`,o.jsx(i,{layout:"centered",of:d,sourceState:"shown"}),`
+`,o.jsx(l,{}),`
+`,o.jsx(e.h2,{id:"sizes",children:"Sizes"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"CompanyLogo"})," component supports the following 4 sizes: ",o.jsx(e.code,{children:"sm"}),", ",o.jsx(e.code,{children:"md"}),", ",o.jsx(e.code,{children:"lg"}),", ",o.jsx(e.code,{children:"xl"}),"."]}),`
+`,o.jsx(i,{layout:"centered",of:c}),`
+`,o.jsx(e.h2,{id:"error-handling",children:"Error Handling"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"CompanyLogo"})," component has built-in error handling. If the logo fails to load, the component will display a fallback icon. An optional prop ",o.jsx(e.code,{children:"shouldHideOnLoadError"})," can be used to control the behavior when the logo fails to load. If this prop is set to ",o.jsx(e.code,{children:"true"}),", the component will not display the logo and will display the fallback icon instead."]}),`
+`,o.jsx(e.h3,{id:"fallback-example",children:"Fallback example"}),`
+`,o.jsx("img",{alt:"Company Logo Error",height:56,src:"./images/company-logo-error.png",width:56}),`
+`,o.jsx(e.h2,{id:"loading",children:"Loading"}),`
+`,o.jsxs(e.p,{children:["The ",o.jsx(e.code,{children:"CompanyLogo"})," component has built-in loading state. If the logo is still loading, the component will display a spinner."]}),`
+`,o.jsx(e.h3,{id:"loading-example",children:"Loading example"}),`
+`,o.jsx("img",{alt:"Company Logo Error",height:56,src:"./images/company-logo-loading.png",width:56})]})}function O(n={}){const{wrapper:e}={...s(),...n.components};return e?o.jsx(e,{...n,children:o.jsx(r,{...n})}):r(n)}export{O as default};
+//# sourceMappingURL=CompanyLogo-Bb3X1kEb.js.map
