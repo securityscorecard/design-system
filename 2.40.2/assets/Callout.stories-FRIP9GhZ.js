500068a3b5d2741669fc1abf9c3355a7b4bb0885
Continuous Integration <username@users.noreply.github.com>
2025-09-29 17:38:25 +0000

    Publish docs for 2.40.2

---
+++ 2.40.2/assets/Callout.stories-FRIP9GhZ.js
@@ -0,0 +1,44 @@
+import{j as r}from"./jsx-runtime-CfatFE5O.js";import{C as e}from"./Callout-CUoaYY0K.js";import{L as D}from"./index-CNIcGMOb.js";import{S as y}from"./index-NGm66y6o.js";import"./index-KN_rxr65.js";import{S as z}from"./index-BJfwPkfN.js";import"./index-ClcD9ViR.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./styled-components.browser.esm-6IU7b-y2.js";import"./constants-l-41uwkI.js";import"./index-BtbvCVen.js";import"./IconWrapper-bBb4ur-x.js";import"./index-_M9_Hem9.js";import"./space.enums-DGrZwzIk.js";import"./_isObject-dmcfxmtS.js";import"./Icon-CMhyohng.js";import"./index-DRm3KUNF.js";import"./index-DbwGX_n3.js";import"./index-DxcpycWO.js";import"./useLogger-D_rFNWpS.js";import"./DSProvider-A0BpKmNN.js";import"./index-_KdZNTQd.js";import"./theme-Dt5JqxPR.js";import"./colors-BJBAFLAc.js";import"./Padbox-DhupY5rO.js";import"./Inline-BnvQG4P8.js";import"./prop-gA1VFyoz.js";import"./index-BiGW80Qt.js";import"./require-router-link-B20G8iu1.js";import"./react-router-dom-Dmb3y85X.js";import"./inheritsLoose-B6jfs0L0.js";import"./setPrototypeOf-DgZC2w_0.js";import"./tiny-invariant-CopsF_GD.js";import"./objectWithoutPropertiesLoose-Dsqj8S3w.js";import"./isNull-C_3YYFwP.js";import"./isNotNull-CploqAht.js";import"./index-DV1EfWHB.js";import"./Inline.enums-KPT5jvjq.js";import"./Cluster-B_3C_5JL.js";const cr={title:"components/Callout",component:e,args:{children:r.jsxs(r.Fragment,{children:[r.jsx(y,{children:"Did you know?"})," This is a callout message that may include ",r.jsx(D,{href:"#",children:"links"})," even!"]})}},t={parameters:{screenshot:{skip:!0}}},a={args:{color:"info"}},s={args:{color:"neutral"}},m={render:o=>r.jsxs(z,{gap:"md",children:[r.jsx(e,{...o,icon:{name:"lightbulb",size:"xs"}}),r.jsx(e,{...o,icon:{name:"lightbulb",size:"sm"}}),r.jsx(e,{...o,icon:{name:"lightbulb",size:"md"}})]}),args:{color:"neutral"}},i={render:o=>r.jsxs(z,{gap:"md",children:[r.jsx(e,{...o,color:"info"}),r.jsx(e,{...o,color:"neutral"})]}),parameters:{themes:{themeOverride:"Dark"}}};var n,p,l;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  parameters: {
+    screenshot: {
+      skip: true
+    }
+  }
+}`,...(l=(p=t.parameters)==null?void 0:p.docs)==null?void 0:l.source}}};var c,u,d;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    color: 'info'
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var g,h,x;s.parameters={...s.parameters,docs:{...(g=s.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    color: 'neutral'
+  }
+}`,...(x=(h=s.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var S,k,C;m.parameters={...m.parameters,docs:{...(S=m.parameters)==null?void 0:S.docs,source:{originalSource:`{
+  render: args => <Stack gap="md">
+      <Callout {...args} icon={{
+      name: 'lightbulb',
+      size: 'xs'
+    }} />
+      <Callout {...args} icon={{
+      name: 'lightbulb',
+      size: 'sm'
+    }} />
+      <Callout {...args} icon={{
+      name: 'lightbulb',
+      size: 'md'
+    }} />
+    </Stack>,
+  args: {
+    color: 'neutral'
+  }
+}`,...(C=(k=m.parameters)==null?void 0:k.docs)==null?void 0:C.source}}};var f,b,j;i.parameters={...i.parameters,docs:{...(f=i.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  render: args => <Stack gap="md">
+      <Callout {...args} color="info" />
+      <Callout {...args} color="neutral" />
+    </Stack>,
+  parameters: {
+    themes: {
+      themeOverride: 'Dark'
+    }
+  }
+}`,...(j=(b=i.parameters)==null?void 0:b.docs)==null?void 0:j.source}}};const ur=["Playground","InfoCallout","NeutralCallout","SizedIcon","ColorsDark"];export{i as ColorsDark,a as InfoCallout,s as NeutralCallout,t as Playground,m as SizedIcon,ur as __namedExportsOrder,cr as default};
+//# sourceMappingURL=Callout.stories-FRIP9GhZ.js.map
